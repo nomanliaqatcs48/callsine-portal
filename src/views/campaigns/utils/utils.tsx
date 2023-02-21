49aae0afcc03f076e6be38b00db7410e047bb3e3
kenvilar <kenvilar@gmail.com>
2023-02-21 11:41:15 +0800

    add _columns utils

---
+++ src/views/campaigns/utils/utils.tsx
@@ -0,0 +1,56 @@
+import moment from "moment";
+
+export const _columns: any = [
+  {
+    Header: "First",
+    accessor: "first_name",
+    Cell: (cell: any) => {
+      return (
+        <a className="" href="">
+          {cell?.value}
+        </a>
+      );
+    },
+  },
+  {
+    Header: "Last",
+    accessor: "last_name",
+  },
+  {
+    Header: "Email",
+    accessor: "email",
+    width: 250,
+    minWidth: 250,
+    Cell: (cell: any) => {
+      return (
+        <a
+          href={`mailto:${cell?.value}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {cell?.value}
+        </a>
+      );
+    },
+  },
+  {
+    Header: "Date Added",
+    accessor: "created_date",
+    width: 250,
+    minWidth: 250,
+    Cell: (cell: any) => {
+      if (!cell?.value) return "";
+      return moment.utc(cell?.value).format("MMMM D, YYYY");
+    },
+  },
+  {
+    Header: "Date Modified",
+    accessor: "modified_date",
+    width: 250,
+    minWidth: 250,
+    Cell: (cell: any) => {
+      if (!cell?.value) return "";
+      return moment.utc(cell?.value).format("MMMM D, YYYY");
+    },
+  },
+];
