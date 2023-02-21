4ead17ce4a22080000904b55a1f5fffe423c1e62
kenvilar <kenvilar@gmail.com>
2023-02-21 11:40:58 +0800

    add useCampaigns hook

---
+++ src/views/campaigns/hooks/useCampaigns.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+export const useCampaigns = () => {
+  const [personsData, setPersonsData] = useState<any[]>([
+    {
+      first_name: "Ken",
+      last_name: "Vilar",
+      email: "kvilar@unionresolute.com",
+      created_date: "2023-01-01T17:06:11.224927Z",
+      modified_date: "2023-02-15T17:06:11.224927Z",
+    },
+    {
+      first_name: "Roselle",
+      last_name: "Ebarle",
+      email: "rebarle@unionresolute.com",
+      created_date: "2023-01-02T17:06:11.224927Z",
+      modified_date: "2023-02-16T17:06:11.224927Z",
+    },
+    {
+      first_name: "John",
+      last_name: "Doe",
+      email: "jdoe@unionresolute.com",
+      created_date: "2023-01-03T17:06:11.224927Z",
+      modified_date: "2023-02-17T17:06:11.224927Z",
+    },
+  ]);
+  const [total, setTotal] = useState<number>(0);
+  const [filters, setFilters] = useState<any>({
+    limit: 10,
+    offset: 0,
+    currentPage: 1,
+  });
+  const [isLoading, setIsLoading] = useState({
+    onPage: true,
+    table: false,
+  });
+
+  return {
+    personsData,
+    setPersonsData,
+    total,
+    setTotal,
+    filters,
+    setFilters,
+    isLoading,
+    setIsLoading,
+  };
+};
