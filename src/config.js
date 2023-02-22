52c08708c3df4881740febbe2715a838f7d22a04
kenvilar <kenvilar@gmail.com>
2023-02-22 14:18:51 +0800

    add process.env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+let env = process.env;
 const config = {
   // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
   // like '/berry-material-react/react/default'
@@ -5,6 +6,10 @@ const config = {
   defaultPath: "/",
   fontFamily: `'Roboto', sans-serif`,
   borderRadius: 12,
+  service: {
+    BASE_URL: env.REACT_APP_SERVICE_BASE_URL,
+  },
+  node_env: env.NODE_ENV,
 };
 
 export default config;
