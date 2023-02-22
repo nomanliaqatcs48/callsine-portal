b4c30cc275a98397120ab3346e8f1cb190258818
kenvilar <kenvilar@gmail.com>
2023-02-22 14:22:07 +0800

    add axios request and response

---
+++ src/services/axios.tsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+import config from "../config";
+
+axios.defaults.baseURL = config.service.BASE_URL; //BASE URL
+axios.defaults.headers.get["Accept"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+
+axios.interceptors.request.use(
+  (config: any) => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (res) => {
+    // console.log("test res", res);
+    return res;
+  },
+  async (err) => {
+    const originalConfig = err.config;
+    // console.log("test err", err);
+    // console.log("test originalConfig", originalConfig);
+
+    if (
+      !originalConfig.url.includes("/login") &&
+      !originalConfig.url.includes("/refresh") &&
+      !originalConfig.url.includes("/token") &&
+      !originalConfig._retry &&
+      err.response
+    ) {
+      // Access Token was expired
+      if (err.response.status === 401) {
+        originalConfig._retry = true;
+
+        try {
+          const rs: any = await axios.post("/token/refresh/", {
+            refresh: localStorage.getItem("refresh"),
+          });
+          localStorage.setItem("token", rs.data.access);
+          originalConfig.headers["Authorization"] = `Bearer ${rs.data.access}`;
+          return axios(originalConfig);
+        } catch (_error: any) {
+          console.log(_error.toJSON());
+          Object.keys(localStorage).forEach(function (key) {
+            localStorage.removeItem(key);
+          });
+          window.location.href = "/login";
+          return Promise.reject(_error);
+        }
+      }
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+const http = {
+  request: axios,
+  create: axios.create,
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  patch: axios.patch,
+};
+
+export default http;
