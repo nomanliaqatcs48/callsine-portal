bec039924414a5a447e65197ae6e774e61f72b4c
kenvilar <kenvilar@gmail.com>
2023-02-23 15:48:22 +0800

    fix loginService

--- src/services/auth.service.tsx
+++ src/services/auth.service.tsx
@@ -1,9 +1,8 @@
-import axios from "axios";
 import http from "./axios";
 import { endpoints } from "./endpoints";
 
 export const loginService = async (data: any) => {
-  return await axios.post(`${endpoints.LOGIN}`, data);
+  return await http.post(`${endpoints.LOGIN}`, data);
 };
 
 export const signupService = async (data: any) => {
