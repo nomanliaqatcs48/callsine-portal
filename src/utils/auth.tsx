2d8075514f3e9ddaa1600a7950c7b12affbd52ea
kenvilar <kenvilar@gmail.com>
2023-02-23 22:50:04 +0800

    fix checkIfAuthenticated

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -1,7 +1,9 @@
 import { loadString } from "./storage";
 
-export const checkIfAuthenticated = async () => {
+export const checkIfAuthenticated = async (): Promise<boolean> => {
   let _auth = await loadString("isAuthenticated");
   let _access = await loadString("access");
+  console.log("_auth", _auth);
+  console.log("_access", _access);
   return !!(_auth && _access);
 };
