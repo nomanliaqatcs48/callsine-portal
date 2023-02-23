bd0da56112d6c9472284117f4d9adf83f6180ac5
kenvilar <kenvilar@gmail.com>
2023-02-23 16:04:09 +0800

    minor change

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -15,7 +15,7 @@ import {
 import Breadcrumbs from "../../ui-component/extended/Breadcrumbs";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-import Customization from "../Customization";
+// import Customization from "../Customization";
 import navigation from "../../menu-items";
 import { drawerWidth } from "../../store/constant";
 import { SET_MENU } from "../../store/actions";
