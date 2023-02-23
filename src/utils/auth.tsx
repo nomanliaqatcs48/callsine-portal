57b0326e1a0161a1e5283eab7ff7cb60013ae292
kenvilar <kenvilar@gmail.com>
2023-02-23 22:30:48 +0800

    modify checkIfAuthenticated

--- src/utils/auth.tsx
+++ src/utils/auth.tsx
@@ -1,7 +1,7 @@
 import { loadString } from "./storage";
 
-export const checkIfAuthenticated = async () => {
-  let _auth = await loadString("isAuthenticated");
-  let _access = await loadString("access");
+export const checkIfAuthenticated = () => {
+  let _auth: any = loadString("isAuthenticated");
+  let _access: any = loadString("access");
   return !!(_auth && _access);
 };
