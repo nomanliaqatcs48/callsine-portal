3ee6b1c7f43eeeea3226f776b5fefcab7a5847d7
kenvilar <kenvilar@gmail.com>
2023-02-24 23:56:42 +0800

    add CampaignDetailPage initial tab

--- src/views/campaigns/detail/index.tsx
+++ src/views/campaigns/detail/index.tsx
@@ -1,11 +1,44 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Box, Tab, Tabs } from "@mui/material";
 import MainCard from "../../../ui-component/cards/MainCard";
+import TabPanel from "../../../ui-component/tabs/TabPanel";
 
 const CampaignDetailPage = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const a11yProps = (index: number) => {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  };
+
   return (
     <MainCard title="Campaign Detail">
-      <Typography variant="body2" />
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
     </MainCard>
   );
 };
