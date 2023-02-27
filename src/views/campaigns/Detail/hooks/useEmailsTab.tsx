9b56fcd890082aa1f738c60cb3b30b11f497b543
kenvilar <kenvilar@gmail.com>
2023-02-27 23:27:45 +0800

    add useEmailsTab hook

---
+++ src/views/campaigns/Detail/hooks/useEmailsTab.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import moment from "moment/moment";
+
+export const useEmailsTab = () => {
+  const _columns: any = [
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "Step",
+      accessor: "step",
+    },
+    {
+      Header: "Stage",
+      accessor: "stage",
+    },
+    {
+      Header: "Status",
+      accessor: "status",
+    },
+    {
+      Header: "Stats",
+      accessor: "stats",
+    },
+    {
+      Header: "Assignee",
+      accessor: "assignee",
+    },
+    {
+      Header: "Owner",
+      accessor: "owner",
+    },
+    {
+      Header: "Last Contacted",
+      accessor: "last_contacted",
+      Cell: (cell: any) => {
+        if (!cell?.value) return "";
+        return moment.utc(cell?.value).format("MMMM D, YYYY");
+      },
+    },
+  ];
+
+  return { _columns };
+};
