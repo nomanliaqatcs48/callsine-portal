7c29ad8d95fcb177db619335d282f3fb0c74e8c3
kenvilar <kenvilar@gmail.com>
2023-02-27 23:10:08 +0800

    minor fix

--- src/ui-component/tabs/TabPanel.tsx
+++ src/ui-component/tabs/TabPanel.tsx
@@ -20,7 +20,7 @@ const TabPanel = (props: TabPanelProps) => {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          <div>{children}</div>
         </Box>
       )}
     </div>
