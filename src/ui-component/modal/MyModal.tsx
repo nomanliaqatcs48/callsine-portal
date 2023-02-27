a325d95d536f63608231a0aa6e3fb6183c728756
kenvilar <kenvilar@gmail.com>
2023-02-27 14:27:00 +0800

    add MyModal component

---
+++ src/ui-component/modal/MyModal.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Box, Button, DialogActions, Modal, Typography } from "@mui/material";
+import Select from "react-select";
+import { ErrorMessage } from "@hookform/error-message";
+
+type MyModal = {
+  open: boolean;
+  handleClose: any;
+  labelledby: string;
+  describedby: string;
+  children: React.ReactNode;
+};
+
+const MyModal = ({
+  open,
+  handleClose,
+  labelledby,
+  describedby,
+  children,
+}: MyModal) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby={labelledby}
+      aria-describedby={describedby}
+    >
+      <Box sx={style} style={{ borderRadius: 4 }}>
+        {children}
+      </Box>
+    </Modal>
+  );
+};
+
+export default MyModal;
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 3,
+  pb: 0,
+};
