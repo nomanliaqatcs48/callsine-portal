30a4b22fc25593521155e311f83145ab7efae78b
kenvilar <kenvilar@gmail.com>
2023-02-28 15:53:48 +0800

    initial cards for sales pitch and followup

--- src/views/persons/Detail/components/Playbook.tsx
+++ src/views/persons/Detail/components/Playbook.tsx
@@ -1,10 +1,53 @@
 import React from "react";
+import { Card, CardContent, Grid, Paper, Typography } from "@mui/material";
 
 const Playbook = () => {
   return (
-    <div>
-      <div>Playbook</div>
-    </div>
+    <Grid container spacing={3}>
+      <Grid item xs />
+      <Grid item xs={6}>
+        <Paper elevation={3}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Sales Pitch
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Paper>
+
+        <div style={{ height: 15 }} />
+
+        <Paper elevation={3}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                Follow up
+              </Typography>
+              <Typography variant="body2">
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Paper>
+      </Grid>
+      <Grid item xs />
+    </Grid>
   );
 };
 
