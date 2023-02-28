d267376495f7507b203a41b2ebec21829bdf9758
kenvilar <kenvilar@gmail.com>
2023-02-28 16:02:15 +0800

    add getPersonDetail to Playbook

--- src/views/persons/Detail/components/Playbook.tsx
+++ src/views/persons/Detail/components/Playbook.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardContent, Grid, Paper, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+import { personDetailService } from "../../../../services/persons.service";
+import { devLog, devLogError } from "../../../../helpers/logs";
 
 const Playbook = () => {
+  const { id } = useParams();
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<any>({
+    onPage: true,
+  });
+
+  useEffect(() => {
+    getPersonDetail();
+  }, []);
+
+  const getPersonDetail = async () => {
+    try {
+      let res = await personDetailService(Number(id));
+      devLog(res);
+      if (res?.data) {
+        setData(res.data);
+        setIsLoading((prev: any) => ({ ...prev, onPage: false }));
+      }
+    } catch (e: any) {
+      devLogError(e.response);
+      setIsLoading((prev: any) => ({ ...prev, onPage: false }));
+    }
+  };
+
   const RenderCard = ({ header, content }: any) => {
     return (
       <Paper elevation={3}>
