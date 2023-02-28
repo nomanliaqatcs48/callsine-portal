b46680054605157c5382c2c830d06abb3a805880
kenvilar <kenvilar@gmail.com>
2023-02-28 14:33:33 +0800

    modify table for person's table

--- src/views/persons/Detail/components/Overview.tsx
+++ src/views/persons/Detail/components/Overview.tsx
@@ -1,10 +1,48 @@
+import {
+  Grid,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Overview = () => {
   return (
-    <div>
-      <div>Overview</div>
-    </div>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={1} />
+        <Grid item xs={6}>
+          <TableContainer component={Paper}>
+            <Table aria-label="simple table">
+              <TableBody>
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    <Typography variant="h5">Header</Typography>
+                  </TableCell>
+                  <TableCell>Value</TableCell>
+                </TableRow>
+
+                <TableRow
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    <Typography variant="h5">Header</Typography>
+                  </TableCell>
+                  <TableCell>Value</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid item xs={3} />
+      </Grid>
+    </>
   );
 };
 
