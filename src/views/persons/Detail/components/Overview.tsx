c5aeaa97444b2c3ebf63f76ed4985e32cc8e4c93
kenvilar <kenvilar@gmail.com>
2023-02-28 14:13:47 +0800

    add tab components for person detail page

---
+++ src/views/persons/Detail/components/Overview.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Overview = () => {
+  return (
+    <div>
+      <div>Overview</div>
+    </div>
+  );
+};
+
+export default Overview;
---
+++ src/views/persons/Detail/components/Playbook.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Playbook = () => {
+  return (
+    <div>
+      <div>Playbook</div>
+    </div>
+  );
+};
+
+export default Playbook;
