7e5b1884696a7f02ac5d708a992e10661091a663
kenvilar <kenvilar@gmail.com>
2023-02-28 15:56:42 +0800

    create RenderCard function

--- src/views/persons/Detail/components/Playbook.tsx
+++ src/views/persons/Detail/components/Playbook.tsx
@@ -2,49 +2,52 @@ import React from "react";
 import { Card, CardContent, Grid, Paper, Typography } from "@mui/material";
 
 const Playbook = () => {
+  const RenderCard = ({ header, content }: any) => {
+    return (
+      <Paper elevation={3}>
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {header}
+            </Typography>
+            <Typography variant="body2">{content}</Typography>
+          </CardContent>
+        </Card>
+      </Paper>
+    );
+  };
+
   return (
     <Grid container spacing={3}>
       <Grid item xs />
       <Grid item xs={6}>
-        <Paper elevation={3}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Sales Pitch
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Paper>
+        <RenderCard
+          header="Sales Pitch"
+          content={
+            <>
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </>
+          }
+        />
 
         <div style={{ height: 15 }} />
 
-        <Paper elevation={3}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography
-                sx={{ fontSize: 14 }}
-                color="text.secondary"
-                gutterBottom
-              >
-                Follow up
-              </Typography>
-              <Typography variant="body2">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Paper>
+        <RenderCard
+          header="Follow up"
+          content={
+            <>
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </>
+          }
+        />
       </Grid>
       <Grid item xs />
     </Grid>
