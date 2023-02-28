22107c97d89f2bc4a5c20e7be2ee199f2b44170a
kenvilar <kenvilar@gmail.com>
2023-02-28 17:51:47 +0800

    add copy button

--- src/views/persons/Detail/components/Playbook.tsx
+++ src/views/persons/Detail/components/Playbook.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import {
+  Avatar,
   Button,
+  ButtonBase,
   Card,
   CardContent,
   Grid,
@@ -15,8 +17,11 @@ import {
 import { devLog, devLogError } from "../../../../helpers/logs";
 import { LoadingButton } from "@mui/lab";
 import xss from "xss";
+import { IconCopy } from "@tabler/icons-react";
+import { useTheme } from "@mui/material/styles";
 
 const Playbook = () => {
+  const theme: any = useTheme();
   const { id } = useParams();
   const [data, setData] = useState<any>(null);
   const [isLoading, setIsLoading] = useState<any>({
@@ -86,13 +91,41 @@ const Playbook = () => {
       <Paper elevation={3}>
         <Card sx={{ minWidth: 275 }}>
           <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
             >
-              {header}
-            </Typography>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {header}
+              </Typography>
+
+              <ButtonBase sx={{ borderRadius: "12px", overflow: "hidden" }}>
+                <Avatar
+                  variant="rounded"
+                  sx={{
+                    ...theme.typography.commonAvatar,
+                    ...theme.typography.mediumAvatar,
+                    transition: "all .2s ease-in-out",
+                    background: theme.palette.secondary.light,
+                    color: theme.palette.secondary.dark,
+                    "&:hover": {
+                      background: theme.palette.secondary.dark,
+                      color: theme.palette.secondary.light,
+                    },
+                  }}
+                  onClick={() => null}
+                  color="inherit"
+                >
+                  <IconCopy stroke={1.5} size="1.3rem" />
+                </Avatar>
+              </ButtonBase>
+            </Grid>
             <Typography
               variant="body2"
               className={class_name}
