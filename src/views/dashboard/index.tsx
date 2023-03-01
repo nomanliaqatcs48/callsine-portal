a9552b03e53f5d2267ab439d840df5088ef186ed
kenvilar <kenvilar@gmail.com>
2023-03-01 15:40:45 +0800

    integrate RemainingClicks to dashboard

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,10 +1,27 @@
-import { Typography } from "@mui/material";
-import MainCard from "../../ui-component/cards/MainCard";
+import { useEffect, useState } from "react";
+import { Grid } from "@mui/material";
 
-const DashboardPage = () => (
-  <MainCard title="Dashboard">
-    <Typography variant="body2" />
-  </MainCard>
-);
+import { gridSpacing } from "../../store/constant";
+import RemainingClicks from "../../ui-component/cards/RemainingClicks";
+
+const DashboardPage = () => {
+  const [isLoading, setIsLoading] = useState<any>({
+    onPage: true,
+  });
+  useEffect(() => {
+    setIsLoading((prev: any) => ({ ...prev, onPage: false }));
+  }, []);
+  return (
+    <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <Grid container spacing={gridSpacing}>
+          <Grid item lg={4} md={6} sm={6} xs={12}>
+            <RemainingClicks isLoading={isLoading?.onPage} value={90} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default DashboardPage;
