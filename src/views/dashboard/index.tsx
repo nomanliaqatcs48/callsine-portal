1d8c4dd371a71cb8cbdda4dcb07885a695ff0568
kenvilar <kenvilar@gmail.com>
2023-03-01 15:42:32 +0800

    dynamic value for clicks_remaining

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -3,20 +3,27 @@ import { Grid } from "@mui/material";
 
 import { gridSpacing } from "../../store/constant";
 import RemainingClicks from "../../ui-component/cards/RemainingClicks";
+import { useAuth } from "../../contexts/auth";
 
 const DashboardPage = () => {
+  const auth: any = useAuth();
   const [isLoading, setIsLoading] = useState<any>({
     onPage: true,
   });
+
   useEffect(() => {
     setIsLoading((prev: any) => ({ ...prev, onPage: false }));
   }, []);
+
   return (
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
         <Grid container spacing={gridSpacing}>
           <Grid item lg={4} md={6} sm={6} xs={12}>
-            <RemainingClicks isLoading={isLoading?.onPage} value={90} />
+            <RemainingClicks
+              isLoading={isLoading?.onPage}
+              value={auth?.clicks_remaining}
+            />
           </Grid>
         </Grid>
       </Grid>
