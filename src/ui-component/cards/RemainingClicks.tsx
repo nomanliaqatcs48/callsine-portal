8cfea8a9f7260a872705b5c5bc4fbab6f6504319
kenvilar <kenvilar@gmail.com>
2023-03-01 15:40:30 +0800

    add RemainingClicks component

---
+++ src/ui-component/cards/RemainingClicks.tsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+
+// material-ui
+import { styled, useTheme } from "@mui/material/styles";
+import { Avatar, Box, Grid, Menu, MenuItem, Typography } from "@mui/material";
+
+// project imports
+import MainCard from "../../ui-component/cards/MainCard";
+import SkeletonEarningCard from "../../ui-component/cards/Skeleton/EarningCard";
+
+// assets
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { IconHandClick } from "@tabler/icons-react";
+
+const CardWrapper = styled(MainCard)(({ theme }: any) => ({
+  backgroundColor: theme.palette.secondary.dark,
+  color: "#fff",
+  overflow: "hidden",
+  position: "relative",
+  "&:after": {
+    content: '""',
+    position: "absolute",
+    width: 210,
+    height: 210,
+    background: theme.palette.secondary[800],
+    borderRadius: "50%",
+    top: -85,
+    right: -95,
+    [theme.breakpoints.down("sm")]: {
+      top: -105,
+      right: -140,
+    },
+  },
+  "&:before": {
+    content: '""',
+    position: "absolute",
+    width: 210,
+    height: 210,
+    background: theme.palette.secondary[800],
+    borderRadius: "50%",
+    top: -125,
+    right: -15,
+    opacity: 0.5,
+    [theme.breakpoints.down("sm")]: {
+      top: -155,
+      right: -70,
+    },
+  },
+}));
+
+// ===========================|| DASHBOARD DEFAULT - EARNING CARD ||=========================== //
+type RemainingClicksTypes = {
+  isLoading: boolean;
+  value: string | number;
+};
+
+const RemainingClicks = ({ isLoading, value }: RemainingClicksTypes) => {
+  const theme: any = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <SkeletonEarningCard />
+      ) : (
+        <CardWrapper border={false} content={false}>
+          <Box sx={{ p: 2.25 }}>
+            <Grid container direction="column">
+              <Grid item>
+                <Grid container justifyContent="space-between">
+                  <Grid item>
+                    <Avatar
+                      variant="rounded"
+                      sx={{
+                        ...theme.typography.commonAvatar,
+                        ...theme.typography.largeAvatar,
+                        backgroundColor: theme.palette.secondary[800],
+                        mt: 1,
+                      }}
+                    >
+                      <IconHandClick size={20} strokeWidth={2} color="white" />
+                    </Avatar>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container alignItems="center">
+                  <Grid item>
+                    <Typography
+                      sx={{
+                        fontSize: "2.125rem",
+                        fontWeight: 500,
+                        mr: 1,
+                        mt: 1.75,
+                        mb: 0.75,
+                      }}
+                    >
+                      {value}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item sx={{ mb: 1.25 }}>
+                <Typography
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: 500,
+                    color: theme.palette.secondary[200],
+                  }}
+                >
+                  Remaining Clicks
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardWrapper>
+      )}
+    </>
+  );
+};
+
+export default RemainingClicks;
