ff3be08cc95d86d3433fa465be54ff7413208d58
kenvilar <kenvilar@gmail.com>
2023-03-02 14:10:44 +0800

    make Editor content value as preformatted

--- src/ui-component/editor/MyEditor.tsx
+++ src/ui-component/editor/MyEditor.tsx
@@ -13,7 +13,10 @@ const MyEditor = ({ initialValue, onEditorChange }: MyEditorTypes) => {
     <Editor
       apiKey="w9b8q1k5xygg3uyj0vqhk0w9lgn1xog0f4auzbg5h8dc8ql6"
       onInit={(evt: any, editor: any) => (editorRef.current = editor)}
-      initialValue={initialValue || ""}
+      initialValue={
+        `<pre style="font-family: Helvetica,Arial,sans-serif">${initialValue}</pre>` ||
+        ""
+      }
       init={{
         height: 500,
         menubar: true,
