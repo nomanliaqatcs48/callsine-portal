a239c317326691adffc887933ac0270c576c42fa
kenvilar <kenvilar@gmail.com>
2023-03-29 17:16:49 +0800

    add personId param

--- src/services/emails.service.tsx
+++ src/services/emails.service.tsx
@@ -2,26 +2,31 @@ import http from "./axios";
 import { endpoints } from "./endpoints";
 
 export const getEmailsService = async (
+  personId: number,
   filters: any,
   searchValue: string = ""
 ) => {
   return await http.get(
-    `${endpoints.EMAILS}?limit=${filters.limit}&offset=${filters.offset}&search=${searchValue}`
+    `${endpoints.EMAILS}?person=${personId}&limit=${filters.limit}&offset=${filters.offset}&search=${searchValue}`
   );
 };
 
-export const getEmailDetailService = async (id: number) => {
-  return await http.get(`${endpoints.EMAILS}/${id}/`);
+export const getEmailDetailService = async (id: number, personId: number) => {
+  return await http.get(`${endpoints.EMAILS}${id}/?person=${personId}`);
 };
 
-export const createEmailService = async (data: any) => {
-  return await http.post(`${endpoints.EMAILS}`, data);
+export const createEmailService = async (personId: number, data: any) => {
+  return await http.post(`${endpoints.EMAILS}?person=${personId}`, data);
 };
 
-export const updateEmailService = async (id: number, data: any) => {
-  return await http.put(`${endpoints.EMAILS}${id}/`, data);
+export const updateEmailService = async (
+  personId: number,
+  id: number,
+  data: any
+) => {
+  return await http.put(`${endpoints.EMAILS}${id}/?person=${personId}`, data);
 };
 
-export const deleteEmailService = async (id: number) => {
-  return await http.delete(`${endpoints.EMAILS}${id}/`);
+export const deleteEmailService = async (personId: number, id: number) => {
+  return await http.delete(`${endpoints.EMAILS}${id}/?person=${personId}`);
 };
