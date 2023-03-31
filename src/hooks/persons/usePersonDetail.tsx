67866aab61f7c235e3b316db4bbb531710b9b151
kenvilar <kenvilar@gmail.com>
2023-04-01 00:03:03 +0800

    create usePersonDetail hook

---
+++ src/hooks/persons/usePersonDetail.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { getPersonDetailService } from "../../services/persons.service";
+import { devLogError } from "../../helpers/logs";
+import { useParams } from "react-router-dom";
+
+export const usePersonDetail = () => {
+  const { id } = useParams();
+  const [data, setData] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<any>({
+    onPage: true,
+  });
+
+  useEffect(() => {
+    getPersonDetail();
+  }, []);
+
+  const getPersonDetail = async () => {
+    try {
+      let res = await getPersonDetailService(Number(id));
+      if (res?.data) {
+        setData(res.data);
+        setIsLoading((prev: any) => ({ ...prev, onPage: false }));
+      }
+    } catch (e: any) {
+      devLogError(e.response);
+      setIsLoading((prev: any) => ({ ...prev, onPage: false }));
+    }
+  };
+
+  return {
+    id,
+    data,
+    setData,
+    isLoading,
+    setIsLoading,
+    getPersonDetail,
+  };
+};
