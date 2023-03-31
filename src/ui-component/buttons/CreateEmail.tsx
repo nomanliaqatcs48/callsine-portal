83511c09b6dbcab55a323568a7762ad9ac55f12f
kenvilar <kenvilar@gmail.com>
2023-03-31 16:15:38 +0800

    add new input fields

--- src/ui-component/buttons/CreateEmail.tsx
+++ src/ui-component/buttons/CreateEmail.tsx
@@ -8,13 +8,19 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  FormControl,
   FormHelperText,
   Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
 } from "@mui/material";
 import xss from "xss";
 import { gridSpacing } from "../../store/constant";
 import { ErrorMessage } from "@hookform/error-message";
 import { devLog } from "../../helpers/logs";
+import { emailAddressPattern } from "../../helpers/forms";
 
 type CreateEmailTypes = {
   html_message: any;
@@ -103,6 +109,123 @@ const CreateEmail = ({
               <Grid item xs={12}>
                 <Box component="form" noValidate autoComplete="off">
                   <div>
+                    <FormControl
+                      fullWidth
+                      error={!!errors.in_reply_to}
+                      margin="dense"
+                      required
+                      disabled={isLoading?.form}
+                    >
+                      <InputLabel id="in_reply_to">Parent Email</InputLabel>
+                      <Select
+                        labelId="in_reply_to"
+                        id="in_reply_to"
+                        label="Parent Email"
+                        defaultValue={""}
+                        onChange={() => null}
+                      >
+                        <MenuItem value="parent_email_1">
+                          Parent Email 1
+                        </MenuItem>
+                      </Select>
+                    </FormControl>
+                    <ErrorMessage
+                      errors={errors}
+                      name="in_reply_to"
+                      render={({ message }) => (
+                        <FormHelperText sx={{ color: "error.main" }}>
+                          {message}
+                        </FormHelperText>
+                      )}
+                    />
+                  </div>
+
+                  <div>
+                    <FormControl
+                      fullWidth
+                      error={!!errors.from_email}
+                      margin="dense"
+                      required
+                      disabled={isLoading?.form}
+                    >
+                      <InputLabel id="from_email">From:</InputLabel>
+                      <Select
+                        labelId="from_email"
+                        id="from_email"
+                        label="From:"
+                        defaultValue={""}
+                        onChange={() => null}
+                      >
+                        <MenuItem value="from_email_1">From Email 1</MenuItem>
+                      </Select>
+                    </FormControl>
+                    <ErrorMessage
+                      errors={errors}
+                      name="from_email"
+                      render={({ message }) => (
+                        <FormHelperText sx={{ color: "error.main" }}>
+                          {message}
+                        </FormHelperText>
+                      )}
+                    />
+                  </div>
+
+                  <div>
+                    <TextField
+                      error={!!errors.to}
+                      disabled={isLoading?.form}
+                      required
+                      margin="dense"
+                      id="to"
+                      label="To:"
+                      type="email"
+                      defaultValue={""}
+                      fullWidth
+                      {...register("to", {
+                        required: "This is required field.",
+                        pattern: emailAddressPattern,
+                      })}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="to"
+                      render={({ message }) => (
+                        <FormHelperText sx={{ color: "error.main" }}>
+                          {message}
+                        </FormHelperText>
+                      )}
+                    />
+                  </div>
+
+                  <div>
+                    <TextField
+                      error={!!errors.subject}
+                      disabled={isLoading?.form}
+                      required
+                      margin="dense"
+                      id="subject"
+                      label="Subject"
+                      type="text"
+                      defaultValue={""}
+                      fullWidth
+                      {...register("subject", {
+                        required: "This is required field.",
+                      })}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="subject"
+                      render={({ message }) => (
+                        <FormHelperText sx={{ color: "error.main" }}>
+                          {message}
+                        </FormHelperText>
+                      )}
+                    />
+                  </div>
+
+                  <div style={{ marginTop: 5 }} />
+
+                  <div>
                     <MyEditor
                       initialValue={getValues("html_message")}
                       onEditorChange={(value: string, editor: any) => {
@@ -114,6 +237,32 @@ const CreateEmail = ({
                     <ErrorMessage
                       errors={errors}
                       name="html_message"
+                      render={({ message }) => (
+                        <FormHelperText sx={{ color: "error.main" }}>
+                          {message}
+                        </FormHelperText>
+                      )}
+                    />
+                  </div>
+
+                  <div>
+                    <TextField
+                      error={!!errors.scheduled_time}
+                      disabled={isLoading?.form}
+                      required
+                      margin="dense"
+                      id="scheduled_time"
+                      label="Scheduled Time"
+                      type="text"
+                      defaultValue={""}
+                      fullWidth
+                      {...register("scheduled_time", {
+                        required: "This is required field.",
+                      })}
+                    />
+                    <ErrorMessage
+                      errors={errors}
+                      name="scheduled_time"
                       render={({ message }) => (
                         <FormHelperText sx={{ color: "error.main" }}>
                           {message}
