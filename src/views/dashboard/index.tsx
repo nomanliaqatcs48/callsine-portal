be1a553ce6a00450525e2551fb6cc9d1235bb1c7
kenvilar <kenvilar@gmail.com>
2023-04-03 16:47:19 +0800

    integrate TotalMailAccounts component to dashboard

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -5,6 +5,7 @@ import { gridSpacing } from "../../store/constant";
 import RemainingClicks from "../../ui-component/cards/RemainingClicks";
 import { useAuth } from "../../contexts/auth";
 import TotalPersonsCard from "../../ui-component/cards/TotalPersons";
+import TotalMailAccounts from "../../ui-component/cards/TotalMailAccounts";
 
 const DashboardPage = () => {
   const auth: any = useAuth();
@@ -28,6 +29,9 @@ const DashboardPage = () => {
           </Grid>
           <Grid item lg={4} md={6} sm={6} xs={12}>
             <TotalPersonsCard isLoading={isLoading?.onPage} />
+          </Grid>
+          <Grid item lg={4} md={6} sm={6} xs={12}>
+            <TotalMailAccounts />
           </Grid>
         </Grid>
       </Grid>
