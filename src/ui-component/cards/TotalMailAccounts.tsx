a83e016a90e459d2781547a76858691c9bf5248c
kenvilar <kenvilar@gmail.com>
2023-04-03 16:44:51 +0800

    add dashboard TotalMailAccounts card

---
+++ src/ui-component/cards/TotalMailAccounts.tsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from "react";
+
+// material-ui
+import { styled, useTheme } from "@mui/material/styles";
+import { Avatar, Box, Grid, Menu, MenuItem, Typography } from "@mui/material";
+
+// project imports
+import MainCard from "../../ui-component/cards/MainCard";
+import SkeletonEarningCard from "../../ui-component/cards/Skeleton/EarningCard";
+
+// assets
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { IconHandClick } from "@tabler/icons-react";
+import { devLogError } from "../../helpers/logs";
+import { useMailAccounts } from "../../hooks/mail-accounts/useMailAccounts";
+
+const CardWrapper = styled(MainCard)(({ theme }: any) => ({
+  backgroundColor: theme.palette.secondary.dark,
+  color: "#fff",
+  overflow: "hidden",
+  position: "relative",
+  "&:after": {
+    content: '""',
+    position: "absolute",
+    width: 210,
+    height: 210,
+    background: theme.palette.secondary[800],
+    borderRadius: "50%",
+    top: -85,
+    right: -95,
+    [theme.breakpoints.down("sm")]: {
+      top: -105,
+      right: -140,
+    },
+  },
+  "&:before": {
+    content: '""',
+    position: "absolute",
+    width: 210,
+    height: 210,
+    background: theme.palette.secondary[800],
+    borderRadius: "50%",
+    top: -125,
+    right: -15,
+    opacity: 0.5,
+    [theme.breakpoints.down("sm")]: {
+      top: -155,
+      right: -70,
+    },
+  },
+}));
+
+const TotalMailAccounts = () => {
+  const theme: any = useTheme();
+  const {
+    mailAccountsData,
+    setMailAccountsData,
+    total,
+    setTotal,
+    searchValue,
+    setSearchValue,
+    filters,
+    setFilters,
+    isLoading,
+    setIsLoading,
+    getMailAccounts,
+  } = useMailAccounts();
+
+  return (
+    <>
+      {isLoading ? (
+        <SkeletonEarningCard />
+      ) : (
+        <CardWrapper border={false} content={false}>
+          <Box sx={{ p: 2.25 }}>
+            <Grid container direction="column">
+              <Grid item>
+                <Grid container justifyContent="space-between">
+                  <Grid item>
+                    <Avatar
+                      variant="rounded"
+                      sx={{
+                        ...theme.typography.commonAvatar,
+                        ...theme.typography.largeAvatar,
+                        backgroundColor: theme.palette.secondary[800],
+                        mt: 1,
+                      }}
+                    >
+                      <IconHandClick size={20} strokeWidth={2} color="white" />
+                    </Avatar>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Grid container alignItems="center">
+                  <Grid item>
+                    <Typography
+                      sx={{
+                        fontSize: "2.125rem",
+                        fontWeight: 500,
+                        mr: 1,
+                        mt: 1.75,
+                        mb: 0.75,
+                      }}
+                    >
+                      {total}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item sx={{ mb: 1.25 }}>
+                <Typography
+                  sx={{
+                    fontSize: "1rem",
+                    fontWeight: 500,
+                    color: theme.palette.secondary[200],
+                  }}
+                >
+                  Mail Accounts
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </CardWrapper>
+      )}
+    </>
+  );
+};
+
+export default TotalMailAccounts;
