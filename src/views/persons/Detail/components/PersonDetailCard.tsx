264021a5abd7286f1147ef153eb9c5d269cbc1a6
kenvilar <kenvilar@gmail.com>
2023-04-14 11:01:09 +0800

    implement icon links

--- src/views/persons/Detail/components/PersonDetailCard.tsx
+++ src/views/persons/Detail/components/PersonDetailCard.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Card, CardContent, Paper, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  IconButton,
+  Paper,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { IconBrandLinkedin, IconLink } from "@tabler/icons-react";
 
 type PersonDetailCardTypes = {
@@ -25,14 +32,47 @@ const PersonDetailCard = ({ data }: PersonDetailCardTypes) => {
                 alignItems: "center",
               }}
             >
-              <IconBrandLinkedin
-                size={32}
-                strokeWidth={2}
-                color="#fff"
-                fill="#0077b5"
-              />
-              <IconLink size={20} strokeWidth={2} color="black" />
-              <IconLink size={20} strokeWidth={2} color="black" />
+              {data?.linkedin && (
+                <Tooltip title="Linkedin">
+                  <IconButton
+                    aria-label="linkedin"
+                    onClick={() => {
+                      window.open(data?.linkedin, "_blank");
+                    }}
+                  >
+                    <IconBrandLinkedin
+                      size={25}
+                      strokeWidth={2}
+                      color="#fff"
+                      fill="#0077b5"
+                    />
+                  </IconButton>
+                </Tooltip>
+              )}
+              {data?.work_email && (
+                <Tooltip title="Work Email">
+                  <IconButton
+                    aria-label="work email"
+                    onClick={() => {
+                      window.open(`mailto:${data?.work_email}`, "_blank");
+                    }}
+                  >
+                    <IconLink size={20} strokeWidth={2} color="black" />
+                  </IconButton>
+                </Tooltip>
+              )}
+              {data?.personal_email && (
+                <Tooltip title="Personal Email">
+                  <IconButton
+                    aria-label="personal email"
+                    onClick={() => {
+                      window.open(`mailto:${data?.personal_email}`, "_blank");
+                    }}
+                  >
+                    <IconLink size={20} strokeWidth={2} color="black" />
+                  </IconButton>
+                </Tooltip>
+              )}
             </div>
             <div>
               <Typography variant="h6" align="center" color="text.secondary">
