abfe9f8041276cdff5d1f9b00dc6ed084c13a1ee
kenvilar <kenvilar@gmail.com>
2023-04-14 09:40:30 +0800

    Implement person detail card

--- src/views/persons/Detail/components/PersonDetailCard.tsx
+++ src/views/persons/Detail/components/PersonDetailCard.tsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { Card, CardContent, Paper, Typography } from "@mui/material";
+import { IconBrandLinkedin, IconLink } from "@tabler/icons-react";
 
-const PersonDetailCard = () => {
-  return <div>This is person detail card</div>;
+type PersonDetailCardTypes = {
+  data: any;
+};
+
+const PersonDetailCard = ({ data }: PersonDetailCardTypes) => {
+  return (
+    <>
+      <Paper elevation={3}>
+        <Card>
+          <CardContent>
+            <Typography variant="h2" align="center" color="text.secondary">
+              {data?.first_name} {data?.last_name}
+            </Typography>
+            <Typography variant="body2" align="center">
+              {data?.job_title}
+            </Typography>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <IconBrandLinkedin
+                size={32}
+                strokeWidth={2}
+                color="#fff"
+                fill="#0077b5"
+              />
+              <IconLink size={20} strokeWidth={2} color="black" />
+              <IconLink size={20} strokeWidth={2} color="black" />
+            </div>
+            <div>
+              <Typography variant="h6" align="center" color="text.secondary">
+                Email Domain: {data?.email_domain}
+              </Typography>
+              <Typography variant="h6" align="center" color="text.secondary">
+                City: {data?.city}
+              </Typography>
+              <Typography variant="h6" align="center" color="text.secondary">
+                State: {data?.state}
+              </Typography>
+            </div>
+          </CardContent>
+        </Card>
+      </Paper>
+    </>
+  );
 };
 
 export default PersonDetailCard;
