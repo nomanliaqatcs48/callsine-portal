8da58a4ed5260f5f352ef73c512628fdea4b0b00
kenvilar <kenvilar@gmail.com>
2023-04-14 10:40:05 +0800

    make getPersonDetailStatService as get request

--- src/services/persons.service.tsx
+++ src/services/persons.service.tsx
@@ -14,7 +14,7 @@ export const updatePersonDetailService = async (id: number, data: any) => {
 };
 
 export const getPersonDetailStatService = async (id: number) => {
-  return await http.put(`${endpoints.PERSON}${id}/stats/`);
+  return await http.get(`${endpoints.PERSON}${id}/stats/`);
 };
 
 export const regeneratePlaybookService = async (id: number) => {
