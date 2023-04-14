543e958d4de050675ac8d13e424977b31b7113ee
kenvilar <kenvilar@gmail.com>
2023-04-14 10:42:42 +0800

    remove overview tab

--- src/views/persons/Detail/index.tsx
+++ src/views/persons/Detail/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Grid, Tab, Tabs } from "@mui/material";
 import MainCard from "../../../ui-component/cards/MainCard";
 import TabPanel from "../../../ui-component/tabs/TabPanel";
-import Overview from "./components/Overview";
 import Playbook from "./components/Playbook";
 import Emails from "./components/Emails";
 import PersonDetailCard from "./components/PersonDetailCard";
@@ -46,19 +45,15 @@ const PersonDetailPage = () => {
           onChange={handleChange}
           aria-label="basic tabs example"
         >
-          <Tab label="Overview" {...a11yProps(0)} />
-          <Tab label="Playbook" {...a11yProps(1)} />
-          <Tab label="Emails" {...a11yProps(2)} />
+          <Tab label="Playbook" {...a11yProps(0)} />
+          <Tab label="Emails" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-        {value === 0 && <Overview />}
+        {value === 0 && <Playbook />}
       </TabPanel>
       <TabPanel value={value} index={1}>
-        {value === 1 && <Playbook />}
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        {value === 2 && <Emails />}
+        {value === 1 && <Emails />}
       </TabPanel>
     </MainCard>
   );
