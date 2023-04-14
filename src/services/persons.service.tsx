672f2e784ea00898124c147cc709c3041c9703f8
kenvilar <kenvilar@gmail.com>
2023-04-14 10:27:09 +0800

    add getPersonDetailStatService

--- src/services/persons.service.tsx
+++ src/services/persons.service.tsx
@@ -13,6 +13,10 @@ export const updatePersonDetailService = async (id: number, data: any) => {
   return await http.put(`${endpoints.PERSON}${id}/`, data);
 };
 
+export const getPersonDetailStatService = async (id: number) => {
+  return await http.put(`${endpoints.PERSON}${id}/stats/`);
+};
+
 export const regeneratePlaybookService = async (id: number) => {
   return await http.post(`${endpoints.PERSON}${id}/create_playbook/`);
 };
