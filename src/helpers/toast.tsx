e9a1cbf2dadb79090b507fdb29d63ae33285c87c
kenvilar <kenvilar@gmail.com>
2023-04-27 17:01:54 +0800

    modify Toast helper

--- src/helpers/toast.tsx
+++ src/helpers/toast.tsx
@@ -1,19 +1,14 @@
 import { toast } from "react-toastify";
 
-export const Toast = () => {
-  const success = (str: string) => {
-    return toast.success(str, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
-  };
-  return {
-    success,
-  };
+export const ToastSuccess = (str: string) => {
+  return toast.success(str, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
 };
