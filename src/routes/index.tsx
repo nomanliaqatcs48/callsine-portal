3c20dab9df1489165eccf650b15819c9942f8670
kenvilar <kenvilar@gmail.com>
2023-05-01 18:46:41 +0800

    remove public routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,9 @@ import { useRoutes } from "react-router-dom";
 // routes
 import MainRoutes from "./MainRoutes";
 import AuthenticationRoutes from "./AuthenticationRoutes";
-import PublicRoutes from "./PublicRoutes";
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-  return useRoutes([MainRoutes, AuthenticationRoutes, PublicRoutes]);
+  return useRoutes([MainRoutes, AuthenticationRoutes]);
 }
