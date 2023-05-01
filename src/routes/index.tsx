bf6b1b60dc651a362e6b8892fc5a8d2f6807da51
kenvilar <kenvilar@gmail.com>
2023-05-01 18:30:46 +0800

    add PublicRoutes in routes index

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
-import { useRoutes } from 'react-router-dom';
+import { useRoutes } from "react-router-dom";
 
 // routes
-import MainRoutes from './MainRoutes';
-import AuthenticationRoutes from './AuthenticationRoutes';
+import MainRoutes from "./MainRoutes";
+import AuthenticationRoutes from "./AuthenticationRoutes";
+import PublicRoutes from "./PublicRoutes";
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
-    return useRoutes([MainRoutes, AuthenticationRoutes]);
+  return useRoutes([MainRoutes, AuthenticationRoutes, PublicRoutes]);
 }
