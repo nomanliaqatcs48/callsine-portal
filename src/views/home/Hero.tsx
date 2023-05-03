960e9a6e039868b34aac5c45061c08085b251060
kenvilar <kenvilar@gmail.com>
2023-05-03 19:40:47 +0800

    separate component for Hero section

---
+++ src/views/home/Hero.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button, Grid, Typography } from "@mui/material";
+
+const Hero = () => {
+  return (
+    <>
+      <Grid container>
+        <Grid item md={2} />
+        <Grid item md={8}>
+          <h1
+            style={{ textAlign: "center", fontSize: 42, lineHeight: "2.5rem" }}
+          >
+            The Fast and Easy Way to{" "}
+            <span style={{ fontWeight: "bold" }}>
+              Find Your Ideal Customers
+            </span>
+          </h1>
+          <Typography fontSize={22} align="center">
+            Looking for a fast and easy way to find the contact information of
+            your ideal customers? Look no further than CallSine, the powerful
+            new Chrome extension that's designed specifically for sales reps.
+          </Typography>
+          <Grid
+            sx={{
+              pt: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button size="large" variant="contained" color="primary">
+              Download CallSine today - it's free
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Hero;
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,27 +1,10 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import Hero from "./Hero";
 
 const Home = () => {
   return (
     <>
-      <Grid container>
-        <Grid item md={2} />
-        <Grid item md={8}>
-          <h1
-            style={{ textAlign: "center", fontSize: 42, lineHeight: "2.5rem" }}
-          >
-            The Fast and Easy Way to{" "}
-            <span style={{ fontWeight: "bold" }}>
-              Find Your Ideal Customers
-            </span>
-          </h1>
-          <Typography fontSize={22} align="center">
-            Looking for a fast and easy way to find the contact information of
-            your ideal customers? Look no further than CallSine, the powerful
-            new Chrome extension that's designed specifically for sales reps.
-          </Typography>
-        </Grid>
-      </Grid>
+      <Hero />
     </>
   );
 };
