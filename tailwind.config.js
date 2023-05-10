62a606d2d005f57613655fd5230455eed84c154a
kenvilar <kenvilar@gmail.com>
2023-05-10 19:54:53 +0800

    extend colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 module.exports = {
   prefix: "tw-",
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1a76d2",
+
+        cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        strongCyan: "hsl(171, 66%, 44%)",
+
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+
+        darkViolet: "hsl(257, 27%, 26%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkViolet: "hsl(260, 8%, 14%) ",
+      },
+    },
   },
   plugins: [],
 };
