4d6c6e73d94c2e3b807ed34ec5c436571be85e1f
kenvilar <kenvilar@gmail.com>
2023-05-10 22:38:44 +0800

    change the default value of material ui breakpoints

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -32,6 +32,16 @@ export const theme = (customization: any) => {
   };
 
   const themeOptions: any = {
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 640,
+        md: 768,
+        lg: 1024,
+        xl: 1280,
+        "2xl": 1536,
+      },
+    },
     direction: "ltr",
     palette: themePalette(themeOption),
     mixins: {
