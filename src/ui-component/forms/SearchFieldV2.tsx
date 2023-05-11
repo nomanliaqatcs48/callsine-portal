d303b4886e28e70ce388a36b42ae1a2abdf8b216
kenvilar <kenvilar@gmail.com>
2023-05-11 19:14:02 +0800

    modify SearchFieldV2 label

--- src/ui-component/forms/SearchFieldV2.tsx
+++ src/ui-component/forms/SearchFieldV2.tsx
@@ -5,7 +5,7 @@ import SearchIcon from "@mui/icons-material/Search";
 const SearchFieldV2 = () => {
   return (
     <>
-      <Typography className="tw-flex tw-flex-col tw-items-center tw-text-xs tw-font-medium">
+      <Typography className="tw-flex tw-flex-col tw-items-center lg:tw-flex-row lg:tw-justify-start tw-text-xs tw-font-medium">
         What are you looking for?
       </Typography>
       <Grid>
