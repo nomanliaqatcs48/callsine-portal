08ebe8ed0a605386af70b620c5a5f8e8e3524eb5
kenvilar <kenvilar@gmail.com>
2023-05-17 16:45:58 +0800

    show hover person detail card

--- src/views/persons/utils/utils.tsx
+++ src/views/persons/utils/utils.tsx
@@ -1,33 +1,206 @@
 import moment from "moment";
 import { useTheme } from "@mui/material/styles";
 import React from "react";
-import { Button } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { IconTrash } from "@tabler/icons-react";
-import { Image } from "mui-image";
 import { ReactComponent as UserIcon } from "../../../assets/images/svg/user.svg";
+import { ReactComponent as FacebookIcon } from "../../../assets/images/svg/facebook.svg";
+import { ReactComponent as LinkedinIcon } from "../../../assets/images/svg/linkedin.svg";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
+import BusinessOutlinedIcon from "@mui/icons-material/BusinessOutlined";
+import FmdGoodOutlinedIcon from "@mui/icons-material/FmdGoodOutlined";
 
 export const _columns: any = () => {
   const theme: any = useTheme();
+
+  const ListItemCustom = ({ icon, text }: any) => {
+    return (
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar className="tw-bg-[#eaeaea] tw-scale-90">{icon}</Avatar>
+        </ListItemAvatar>
+        {text}
+      </ListItem>
+    );
+  };
+
   return [
     {
       Header: "Name",
       accessor: "first_name",
       Cell: (cell: any) => {
-        return (
-          <div className="tw-flex tw-gap-2">
-            {cell?.value && (
-              <>
-                <div className="tw-relative tw-w-7 tw-h-7 tw-overflow-hidden tw-bg-gray-100 tw-rounded-full">
-                  <UserIcon className="tw-absolute tw-w-7 tw-h-7 tw-text-gray-400" />
-                </div>
+        const listItems = [
+          {
+            //name
+            icon: (
+              <PersonOutlineOutlinedIcon
+                className="tw-opacity-80"
+                fontSize="small"
+                sx={{ color: "#151515" }}
+              />
+            ),
+            text: (
+              <span className="tw-opacity-90 tw-text-sm tw-font-medium">
+                Lilian May
+              </span>
+            ),
+          },
+          {
+            //phone
+            icon: (
+              <LocalPhoneOutlinedIcon
+                className="tw-opacity-80"
+                fontSize="small"
+                sx={{ color: "#151515" }}
+              />
+            ),
+            text: (
+              <span className="tw-opacity-90 tw-text-sm tw-font-medium">
+                (555) 555-1234
+              </span>
+            ),
+          },
+          {
+            //email
+            icon: (
+              <AlternateEmailOutlinedIcon
+                className="tw-opacity-80"
+                fontSize="small"
+                sx={{ color: "#151515" }}
+              />
+            ),
+            text: (
+              <span className="tw-opacity-90 tw-text-sm tw-font-medium">
+                johndoe@gmail.com
+              </span>
+            ),
+          },
+          {
+            //job title
+            icon: (
+              <BusinessOutlinedIcon
+                className="tw-opacity-80"
+                fontSize="small"
+                sx={{ color: "#151515" }}
+              />
+            ),
+            text: (
+              <span className="tw-opacity-90 tw-text-sm tw-font-medium">
+                Co-Founder & CEO at Google
+              </span>
+            ),
+          },
+          {
+            //address
+            icon: (
+              <FmdGoodOutlinedIcon
+                className="tw-opacity-80"
+                fontSize="small"
+                sx={{ color: "#151515" }}
+              />
+            ),
+            text: (
+              <span className="tw-opacity-90 tw-text-sm tw-font-medium">
+                San Ramon, CA
+              </span>
+            ),
+          },
+        ];
 
-                <a
-                  className="tw-no-underline tw-flex tw-items-center"
+        return (
+          <div className="">
+            {cell?.value && (
+              <Tooltip
+                title={
+                  <>
+                    {/*icon and name*/}
+                    <div className="tw-flex tw-justify-center tw-items-center tw-gap-x-5">
+                      <div className="tw-relative tw-w-8 tw-h-8 tw-overflow-hidden tw-bg-gray-100 tw-rounded-full">
+                        <UserIcon className="tw-absolute tw-w-8 tw-h-8 tw-text-gray-400" />
+                      </div>
+                      <div>
+                        <span className="tw-font-bold tw-text-[0.80rem]">
+                          {cell?.value || ""}{" "}
+                          {cell?.row?.original?.last_name || ""}
+                        </span>
+                        <div className="tw-text-[#1ea3ce] tw-text-sm tw-font-normal">
+                          {cell?.row?.original?.work_email}
+                        </div>
+                      </div>
+                    </div>
+                    {/*social icons*/}
+                    <div className="tw-flex tw-justify-center tw-items-center tw-gap-6 tw-pt-5 tw-pb-3">
+                      <div className="tw-py-[8px] tw-px-[11px] tw-w-[28px] tw-bg-[#1677f2] tw-rounded-full tw-scale-110">
+                        <FacebookIcon className="" style={{ fill: "white" }} />
+                      </div>
+                      <div className="tw-py-[9px] tw-px-[8px] tw-w-[28px] tw-bg-[#4465a8] tw-rounded-full tw-scale-110">
+                        <LinkedinIcon style={{ fill: "white" }} />
+                      </div>
+                    </div>
+                    {/*list*/}
+                    <List
+                      sx={{
+                        width: "100%",
+                        maxWidth: 350,
+                        bgcolor: "background.paper",
+                      }}
+                    >
+                      {listItems.map((item, idx) => {
+                        return (
+                          <React.Fragment key={idx}>
+                            <Divider variant="fullWidth" />
+                            <ListItemCustom icon={item.icon} text={item.text} />
+                          </React.Fragment>
+                        );
+                      })}
+                    </List>
+                  </>
+                }
+                componentsProps={{
+                  popper: {
+                    sx: {
+                      "&.MuiTooltip-popper": {
+                        borderRadius: "0.5rem",
+                        "& .MuiTooltip-tooltip": {
+                          backgroundColor: theme.palette.common.white,
+                          color: "rgba(0, 0, 0, 0.87)",
+                          borderRadius: "0.5rem",
+                          boxShadow: theme.shadows[1],
+                          fontSize: 11,
+                          paddingY: 2,
+                          paddingX: 0,
+                          width: 351,
+                          maxWidth: 351,
+                        },
+                      },
+                    },
+                  },
+                }}
+              >
+                <Button
+                  variant="text"
+                  className="tw-flex tw-items-center tw-gap-2 tw-text-inherit tw-text-xs tw-no-underline hover:tw-bg-transparent text"
                   href={`/people/${cell?.row?.original?.id}`}
                 >
+                  <div className="tw-relative tw-w-7 tw-h-7 tw-overflow-hidden tw-bg-gray-100 tw-rounded-full">
+                    <UserIcon className="tw-absolute tw-w-7 tw-h-7 tw-text-gray-400" />
+                  </div>
                   {cell?.value || ""} {cell?.row?.original?.last_name || ""}
-                </a>
-              </>
+                </Button>
+              </Tooltip>
             )}
           </div>
         );
