e49e4b64307dd7283c69fa2a11d41b63c1b8e1e6
kenvilar <kenvilar@gmail.com>
2023-05-18 19:13:03 +0800

    update burger icon

--- src/layout/MainLayout/Header/index.tsx
+++ src/layout/MainLayout/Header/index.tsx
@@ -53,17 +53,17 @@ const Header = ({ handleLeftDrawerToggle }: HeaderTypes) => {
               ...theme.typography.commonAvatar,
               ...theme.typography.mediumAvatar,
               transition: "all .2s ease-in-out",
-              background: theme.palette.secondary.light,
-              color: theme.palette.secondary.dark,
+              background: "white", //theme.palette.secondary.light,
+              color: theme.palette.primary.main, //theme.palette.secondary.dark,
               "&:hover": {
-                background: theme.palette.secondary.dark,
-                color: theme.palette.secondary.light,
+                // background: theme.palette.secondary.dark,
+                // color: theme.palette.secondary.light,
               },
             }}
             onClick={handleLeftDrawerToggle}
             color="inherit"
           >
-            <IconMenu2 stroke={1.5} size="1.3rem" />
+            <IconMenu2 stroke={2} size="1.3rem" />
           </Avatar>
         </ButtonBase>
       </Box>
