c6c4852e216d025d05a4fe4f3c06786098e73296
kenvilar <kenvilar@gmail.com>
2023-05-18 19:10:10 +0800

    change borderRadius

--- src/layout/MainLayout/Sidebar/MenuList/NavItem/index.tsx
+++ src/layout/MainLayout/Sidebar/MenuList/NavItem/index.tsx
@@ -91,7 +91,7 @@ const NavItem = ({ item, level }: NavItemTypes) => {
       {...listItemProps}
       disabled={item.disabled}
       sx={{
-        borderRadius: `${customization.borderRadius}px`,
+        borderRadius: 0, //borderRadius: `${customization.borderRadius}px`,
         mb: 0.5,
         alignItems: "flex-start",
         backgroundColor: level > 1 ? "transparent !important" : "inherit",
