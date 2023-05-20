7a6bd33537a68ed86bd3d994240aa73b6998383b
kenvilar <kenvilar@gmail.com>
2023-05-20 22:32:57 +0800

    add new headers for exporting people

--- src/ui-component/buttons/ExportPeople.tsx
+++ src/ui-component/buttons/ExportPeople.tsx
@@ -11,6 +11,20 @@ const ExportPeople = ({ data }: ExportPeopleTypes) => {
   const headers = [
     { label: "First Name", key: "first_name" },
     { label: "Last Name", key: "last_name" },
+    { label: "Title", key: "job_title" },
+    { label: "Company", key: "org.name" },
+    { label: "Work Email", key: "work_email" },
+    { label: "Linkedin", key: "linkedin" },
+    { label: "Industry", key: "org.industry" },
+    { label: "City", key: "city" },
+    { label: "State", key: "state" },
+    { label: "Email Domain", key: "email_domain" },
+    { label: "Employee Count", key: "org.employee_count" },
+    { label: "Revenue Range", key: "org.revenue_range" },
+    { label: "NAICS", key: "org.naics" },
+    { label: "NAICS Description", key: "org.naics_description" },
+    { label: "SIC", key: "org.sic" },
+    { label: "SIC Description", key: "org.sic_description" },
   ];
 
   return (
