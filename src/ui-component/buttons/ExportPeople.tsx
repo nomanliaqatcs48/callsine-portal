14a84a7a559d22ea63461203452be3f7190a2cc7
kenvilar <kenvilar@gmail.com>
2023-05-20 22:41:35 +0800

    add tooltip on export people button

--- src/ui-component/buttons/ExportPeople.tsx
+++ src/ui-component/buttons/ExportPeople.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { CSVLink } from "react-csv";
-import { Button } from "@mui/material";
+import { Button, Tooltip } from "@mui/material";
 import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
 
 type ExportPeopleTypes = {
@@ -28,17 +28,32 @@ const ExportPeople = ({ data }: ExportPeopleTypes) => {
   ];
 
   return (
-    <div>
-      <CSVLink data={data} headers={headers} filename={"people.csv"}>
-        <Button className="tw-text-[#778da9]">
-          <FileDownloadOutlinedIcon
-            sx={{ color: "#778da9", fontSize: 15 }}
-            className="tw-mr-2"
-          />
-          Export People
-        </Button>
+    <>
+      <CSVLink
+        data={data}
+        headers={headers}
+        filename={"people.csv"}
+        onClick={() => {
+          if (!data?.length) {
+            return false;
+          }
+        }}
+      >
+        <Tooltip title={data?.length > 0 ? "Export CSV" : "No data"}>
+          <Button
+            className={`tw-text-[#778da9] ${
+              data?.length > 0 ? "" : "tw-cursor-not-allowed"
+            }`}
+          >
+            <FileDownloadOutlinedIcon
+              sx={{ color: "#778da9", fontSize: 15 }}
+              className="tw-mr-2"
+            />
+            Export People
+          </Button>
+        </Tooltip>
       </CSVLink>
-    </div>
+    </>
   );
 };
 
