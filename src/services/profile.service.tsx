cf7b702f099698cde4a9bd4962e30c2e46e60aa0
kenvilar <kenvilar@gmail.com>
2023-05-23 23:31:03 +0800

    add getUsersMeService

--- src/services/profile.service.tsx
+++ src/services/profile.service.tsx
@@ -5,6 +5,10 @@ export const profileService = async () => {
   return await http.get(`${endpoints.PROFILE}`);
 };
 
+export const getUsersMeService = async () => {
+  return await http.get(`${endpoints.USERS_ME}`);
+};
+
 export const postUsersMeService = async (data: any) => {
   return await http.post(`${endpoints.USERS_ME}`, data);
 };
