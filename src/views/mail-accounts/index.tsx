eda620d3a630fccc4dd9d9d370c68fbdde6edf10
kenvilar <kenvilar@gmail.com>
2023-05-24 15:44:40 +0800

    change the layout of mail accounts table

--- src/views/mail-accounts/index.tsx
+++ src/views/mail-accounts/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Divider, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 import { useAsyncDebounce } from "react-table";
 import { useMailAccounts } from "../../hooks/mail-accounts/useMailAccounts";
@@ -8,6 +8,11 @@ import TotalListSmallCard from "../../ui-component/cards/TotalListSmallCard";
 import SearchField from "../../ui-component/forms/SearchField";
 import MyTable from "../../ui-component/tables/MyTable";
 import { _columns } from "../../utils/mail-accounts/utils";
+import CreateOrEditPerson from "../../ui-component/buttons/CreateOrEditPerson";
+import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
+import ImportPeople from "../../ui-component/buttons/ImportPeople";
+import ExportPeople from "../../ui-component/buttons/ExportPeople";
+import DeleteSelectedPeople from "../../ui-component/buttons/DeleteSelectedPeople";
 
 const MailAccountsPage = () => {
   const {
@@ -39,10 +44,10 @@ const MailAccountsPage = () => {
   }, 1000);
 
   return (
-    <div>
+    <>
       <Typography className="tw-text-4xl">Mail Accounts</Typography>
       <Grid className="tw-my-5" />
-      <Grid
+      {/*<Grid
         container
         direction="row"
         justifyContent="flex-end"
@@ -54,8 +59,8 @@ const MailAccountsPage = () => {
           onClick={() => null}
           btnVariant="outlined"
         />
-      </Grid>
-      <div style={{ height: 3 }} />
+      </Grid>*/}
+      {/*<div style={{ height: 3 }} />*/}
       {/*<Grid container spacing={2}>
         <Grid item xs={12} sm={6} md={4} lg={3}>
           <TotalListSmallCard
@@ -77,26 +82,88 @@ const MailAccountsPage = () => {
           onChange={handleSearchOnBeforeChange}
         />
       </Grid>*/}
-      <MyTable
-        columns={_columns(getMailAccounts)}
-        data={mailAccountsData}
-        totalItems={total || 0}
-        tableName="MailAccounts"
-        tableClassName="table-mail-accounts gray-header table-sm"
-        isTableLoading={isLoading?.table}
-        filters={filters}
-        setFilters={setFilters}
-        removePageSizeDropdown={false}
-        isResponsive={true}
-        removeSelection={false}
-        hiddenColumns={["last_name"]}
-        // setSortedId={setSortedId}
-        // setIsOrderDesc={setIsOrderDesc}
-        //
-        // sortedId={sortedId}
-        // isOrderDesc={isOrderDesc}
-      />
-    </div>
+      <Paper elevation={0} className="tw-pt-4">
+        <Grid container>
+          <Grid item xs={12} lg={2}>
+            <Typography className="tw-text-lg tw-font-medium tw-pl-3">
+              Accounts
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            lg={10}
+            className="tw-flex tw-flex-col lg:tw-flex-row lg:tw-gap-x-1 lg:tw-items-center lg:tw-justify-end lg:tw-px-3"
+          >
+            <Button disableElevation type="button" color="primary">
+              <PersonAddAlt1Icon
+                color="primary"
+                sx={{ fontSize: 15 }}
+                className="tw-mr-2"
+              />
+              Add Users
+            </Button>
+
+            <Divider
+              orientation="vertical"
+              variant="middle"
+              flexItem
+              className="tw-hidden lg:tw-block"
+              sx={{ borderColor: "#bbc6d4", borderRightWidth: 2 }}
+            />
+
+            <ExportPeople data={[]} />
+
+            <Divider
+              orientation="vertical"
+              variant="middle"
+              flexItem
+              className="tw-hidden lg:tw-block"
+              sx={{ borderColor: "#bbc6d4", borderRightWidth: 2 }}
+            />
+
+            <DeleteSelectedPeople selectedPeople={[]} onLoadApi={() => null} />
+
+            <Divider
+              orientation="vertical"
+              variant="middle"
+              flexItem
+              className="tw-hidden lg:tw-block"
+              sx={{ borderColor: "#bbc6d4", borderRightWidth: 2 }}
+            />
+
+            <Button
+              onClick={() => null}
+              disabled={false}
+              variant="text"
+              className="tw-text-[#3fb0d5] hover:tw-bg-transparent focus:tw-bg-transparent active:tw-bg-transparent tw-cursor-auto"
+            >
+              {total || 0} Mail{" "}
+              {total === 0 || total === 1 ? "Account" : "Accounts"}
+            </Button>
+          </Grid>
+        </Grid>
+        <MyTable
+          columns={_columns(getMailAccounts)}
+          data={mailAccountsData}
+          totalItems={total || 0}
+          tableName="MailAccounts"
+          tableClassName="table-mail-accounts gray-header table-sm"
+          isTableLoading={isLoading?.table}
+          filters={filters}
+          setFilters={setFilters}
+          removePageSizeDropdown={false}
+          isResponsive={true}
+          removeSelection={false}
+          hiddenColumns={["last_name"]}
+          // setSortedId={setSortedId}
+          // setIsOrderDesc={setIsOrderDesc}
+          //
+          // sortedId={sortedId}
+          // isOrderDesc={isOrderDesc}
+        />
+      </Paper>
+    </>
   );
 };
 
