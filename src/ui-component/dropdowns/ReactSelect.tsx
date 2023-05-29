8c8b84c105c0824ebb5dcdcbc2c432d4a5f6688b
kenvilar <kenvilar@gmail.com>
2023-05-29 21:35:16 +0800

    modify ReactSelect

--- src/ui-component/dropdowns/ReactSelect.tsx
+++ src/ui-component/dropdowns/ReactSelect.tsx
@@ -2,10 +2,11 @@ import Select from "react-select";
 
 type ReactSelectTypes = {
   name: string;
+  variant: "normal" | "blue";
   [x: string]: any;
 };
 
-const ReactSelect = ({ name, ...props }: ReactSelectTypes) => {
+const ReactSelect = ({ name, variant, ...props }: ReactSelectTypes) => {
   const selectStyles = {
     control: (styles: any) => ({
       ...styles,
@@ -46,19 +47,76 @@ const ReactSelect = ({ name, ...props }: ReactSelectTypes) => {
     singleValue: (styles: any, { data }: any) => ({ ...styles }),
   };
 
+  const selectBlueStyles = {
+    control: (styles: any) => ({
+      ...styles,
+      backgroundColor: "#1a76d2",
+      boxShadow: "none",
+      border: "1px solid #1a76d2",
+      cursor: "pointer",
+    }),
+    option: (styles: any, { data, isDisabled, isFocused, isSelected }: any) => {
+      return {
+        ...styles,
+        backgroundColor: isDisabled
+          ? undefined
+          : isSelected
+          ? "#1a76d2"
+          : isFocused
+          ? "white"
+          : undefined,
+        color: isDisabled
+          ? "#ccc"
+          : isSelected
+          ? "white"
+          : isFocused
+          ? "black"
+          : data.color,
+        cursor: isDisabled ? "not-allowed" : "pointer",
+
+        ":active": {
+          ...styles[":active"],
+          backgroundColor: !isDisabled ? data.color : undefined,
+        },
+      };
+    },
+    input: (styles: any) => ({ ...styles }),
+    placeholder: (styles: any) => ({
+      ...styles,
+      color: "white",
+      fontSize: "0.7rem",
+      fontWeight: 600,
+    }),
+    singleValue: (styles: any, { data }: any) => ({ ...styles }),
+    indicatorSeparator: (
+      styles: any,
+      { isDisabled, isFocused, innerProps }: any
+    ) => ({ ...styles, display: "none" }),
+    dropdownIndicator: (styles: any) => ({
+      ...styles,
+      color: "white",
+      ":hover": {
+        color: "white",
+      },
+      svg: {
+        width: 16,
+      },
+    }),
+  };
+
   return (
     <Select
-      className="basic-single"
+      className="basic-single tw-cursor-pointer"
       classNamePrefix="select"
       name={name}
       defaultValue={""}
       options={[]}
       isDisabled={false}
       isLoading={false}
-      isClearable={false}
+      isClearable={true}
       isSearchable={true}
       isRtl={false}
-      styles={selectStyles}
+      styles={variant === "normal" ? selectStyles : selectBlueStyles}
       onChange={(newValue, actionMeta) => {
         // devLog("Value Changed");
         // devLog(newValue);
@@ -71,6 +129,7 @@ const ReactSelect = ({ name, ...props }: ReactSelectTypes) => {
         // devLog(`action: ${actionMeta.action}`);
         // devLog("===========");
       }}
+      {...props}
     />
   );
 };
