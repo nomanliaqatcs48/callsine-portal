6f3def4024614376b5619c5a460be0f2bcd1f4f0
kenvilar <kenvilar@gmail.com>
2023-05-30 16:13:31 +0800

    implement playbook responses for left portion

--- src/views/persons/Detail/components/v2/Playbook/PlaybookV2.tsx
+++ src/views/persons/Detail/components/v2/Playbook/PlaybookV2.tsx
@@ -3,14 +3,35 @@ import {
   Grid,
   List,
   ListItemButton,
-  ListItemIcon,
   ListItemText,
   Paper,
+  Tooltip,
 } from "@mui/material";
 import ReactSelect from "../../../../../../ui-component/dropdowns/ReactSelect";
+import WatchLaterIcon from "@mui/icons-material/WatchLater";
 
 const PlaybookV2 = () => {
-  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const playbookItems = [
+    {
+      date: "Saturday",
+      subject:
+        "Campaign Campaign lorem ipsum Campaign lorem ipsum Campaign lorem ipsum Campaign",
+      msg: "Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor",
+    },
+    {
+      date: "Saturday",
+      subject:
+        "Campaign Campaign lorem ipsum Campaign lorem ipsum Campaign lorem ipsum Campaign",
+      msg: "Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor",
+    },
+    {
+      date: "Saturday",
+      subject:
+        "Campaign Campaign lorem ipsum Campaign lorem ipsum Campaign lorem ipsum Campaign",
+      msg: "Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor",
+    },
+  ];
 
   const handleListItemClick = (event: MouseEvent, index: number) => {
     setSelectedIndex(index);
@@ -59,19 +80,68 @@ const PlaybookV2 = () => {
               />
             </div>
             <div className="list-container">
-              <List component="nav" aria-label="secondary mailbox folder">
-                <ListItemButton
-                  selected={selectedIndex === 2}
-                  onClick={(event) => handleListItemClick(event, 2)}
-                >
-                  <ListItemText primary="Trash" />
-                </ListItemButton>
-                <ListItemButton
-                  selected={selectedIndex === 3}
-                  onClick={(event) => handleListItemClick(event, 3)}
-                >
-                  <ListItemText primary="Spam" />
-                </ListItemButton>
+              <List
+                component="nav"
+                aria-label="secondary mailbox folder"
+                className="tw-py-0 tw-border-t tw-border-[#e8ecf5]"
+              >
+                {playbookItems.map((item, idx) => {
+                  return (
+                    <ListItemButton
+                      className="tw-transition-all tw-duration-700 tw-ease-linear"
+                      selected={selectedIndex === idx}
+                      onClick={(event) => handleListItemClick(event, idx)}
+                      sx={{
+                        borderBottom: "1px solid #e8ecf5",
+                        "&.Mui-selected": {
+                          backgroundColor: "#f2fafc",
+                          "&:hover": {
+                            backgroundColor: "#f2fafc",
+                          },
+                          "&::before": {
+                            content: '""',
+                            position: "absolute",
+                            borderLeft: "8px solid #0096c7",
+                            height: "100%",
+                            left: 0,
+                          },
+                        },
+                        "&:hover": {
+                          backgroundColor: "#f2fafc",
+                        },
+                      }}
+                    >
+                      <Tooltip title={item.subject}>
+                        <ListItemText
+                          primary={
+                            <>
+                              <div className="tw-text-black tw-font-light tw-text-right tw-text-[0.70rem]">
+                                {item.date}
+                              </div>
+                              <div className="tw-text-black tw-font-semibold tw-truncate tw-mb-1">
+                                {item.subject}
+                              </div>
+                            </>
+                          }
+                          secondary={
+                            <>
+                              <div className="tw-flex">
+                                <div className="tw-w-11/12 tw-truncate tw-text-[#8c9fb7] tw-font-extralight">
+                                  {item.msg}
+                                </div>
+                                <div className="tw-w-1/12 tw-text-right">
+                                  <WatchLaterIcon
+                                    sx={{ fontSize: 15, color: "#1a76d2" }}
+                                  />
+                                </div>
+                              </div>
+                            </>
+                          }
+                        />
+                      </Tooltip>
+                    </ListItemButton>
+                  );
+                })}
               </List>
             </div>
           </Grid>
