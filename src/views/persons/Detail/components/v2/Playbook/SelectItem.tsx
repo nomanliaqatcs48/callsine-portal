71290794ce006f7fc29fc776a2186509985187d5
kenvilar <kenvilar@gmail.com>
2023-06-08 18:44:41 +0800

    minor change

--- src/views/persons/Detail/components/v2/Playbook/SelectItem.tsx
+++ src/views/persons/Detail/components/v2/Playbook/SelectItem.tsx
@@ -5,7 +5,7 @@ type SelectItemTypes = {
 const SelectItem = ({ prompts }: SelectItemTypes) => {
   return (
     <>
-      <div className="tw-flex tw-flex-col tw-justify-center tw-items-center">
+      <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-full">
         <div className="tw-text-xl tw-py-10 tw-text-[#99a9be] tw-font-semibold">
           {prompts?.length === 0 && "Empty data"}
           {prompts?.length > 0 && "Select an email"}
