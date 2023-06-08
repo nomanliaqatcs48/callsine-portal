304528393824f7f929fc64900c60a92c7be400e9
kenvilar <kenvilar@gmail.com>
2023-06-08 18:13:44 +0800

    insert Body Loader

---
+++ src/helpers/loaders.tsx
@@ -0,0 +1,14 @@
+export const insertBodyLoader = () => {
+  let _body: any = document.querySelector("body");
+  if (_body) {
+    _body.classList.toggle("tw-overflow-hidden");
+    _body.insertAdjacentHTML(
+      "afterbegin",
+      '<div class="tw-h-full tw-w-full tw-flex tw-justify-center tw-items-center tw-z-[9999] tw-bg-white tw-opacity-80 tw-absolute tw-overflow-hidden">' +
+        '<div class="tw-w-[40px] tw-h-[40px]">' +
+        '<svg class="tw-block tw-animate-spin" viewBox="22 22 44 44">' +
+        '<circle style="stroke: #1976d2;stroke-dasharray: 80px,200px;stroke-dashoffset: 0;-webkit-animation: animation-1p2h4ri 1.4s ease-in-out infinite;animation: animation-1p2h4ri 1.4s ease-in-out infinite;" cx="44" cy="44" r="20.2" fill="none" stroke-width="3.6"></circle>' +
+        "</svg></div></div>"
+    );
+  }
+};
