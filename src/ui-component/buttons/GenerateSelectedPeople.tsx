c201710d819b4d9643e0e545a7e707aad742779a
kenvilar <kenvilar@gmail.com>
2023-06-12 22:47:54 +0800

    create warning if no selected people to generate

--- src/ui-component/buttons/GenerateSelectedPeople.tsx
+++ src/ui-component/buttons/GenerateSelectedPeople.tsx
@@ -1,8 +1,19 @@
 import AutoModeIcon from "@mui/icons-material/AutoMode";
-import { Button, Tooltip } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import { devLog, devLogError } from "../../helpers/logs";
 import { ToastError, ToastSuccess, ToastWarning } from "../../helpers/toast";
 import { bulkGenerateService } from "../../services/prompts.service";
+import React, { useState } from "react";
+import MyModal from "../modal/MyModal";
+import ReactSelect from "../dropdowns/ReactSelect";
+import { useForm } from "react-hook-form";
 
 type GenerateSelectedPeopleProps = {
   selectedRows: any[];
@@ -13,6 +24,31 @@ const GenerateSelectedPeople = ({
   selectedRows,
   onLoadApi,
 }: GenerateSelectedPeopleProps) => {
+  const [open, setOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = useState<any>({
+    submit: false,
+  });
+  const {
+    register,
+    unregister,
+    setValue,
+    handleSubmit,
+    reset,
+    getValues,
+    trigger,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const handleOpen = () => {
+    if (!selectedRows?.length) {
+      ToastWarning("Please select person");
+      return;
+    }
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
   const onGenerate = async () => {
     devLog("selectedRows", selectedRows);
     if (!selectedRows?.length) {
@@ -69,6 +105,45 @@ const GenerateSelectedPeople = ({
           Generate Selected
         </Button>
       </Tooltip>
+
+      {/*{open && (
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          sx={{ width: { xs: 400 }, marginX: "auto" }}
+          scroll="body"
+          fullWidth={true}
+          maxWidth="xl"
+          aria-labelledby="Generate selected people"
+          aria-describedby="Generate selected people"
+          disableEnforceFocus={true}
+        >
+          <DialogContent className="tw-pt-6">
+            <ReactSelect
+              name="generate-playbook"
+              className="basic-single tw-cursor-pointer"
+              variant="blue"
+              placeholder="GENERATE PLAYBOOK"
+              isClearable={true}
+              isSearchable={true}
+            />
+          </DialogContent>
+          <DialogActions className="tw-pb-6">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={onGenerate}
+              disabled={isLoading?.submit}
+              className="tw-bg-primary hover:tw-bg-primaryDark tw-normal-case"
+            >
+              Yes, please!
+            </Button>
+            <Button onClick={handleClose} disabled={isLoading?.submit}>
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}*/}
     </>
   );
 };
