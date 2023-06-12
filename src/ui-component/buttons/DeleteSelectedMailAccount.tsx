63fcd4b23f6516f83cb6585aee843d31f4f33258
kenvilar <kenvilar@gmail.com>
2023-06-12 22:09:38 +0800

    add delete modal confirmation

--- src/ui-component/buttons/DeleteSelectedMailAccount.tsx
+++ src/ui-component/buttons/DeleteSelectedMailAccount.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Button, Tooltip } from "@mui/material";
+import { Button, DialogActions, Tooltip, Typography } from "@mui/material";
 import { devLog, devLogError } from "../../helpers/logs";
 import { ToastError, ToastSuccess, ToastWarning } from "../../helpers/toast";
 import { deleteMailAccountService } from "../../services/mail-accounts.service";
+import MyModal from "../modal/MyModal";
+import { LoadingButton } from "@mui/lab";
 
 type DeleteSelectedMailAccountsTypes = {
   selectedRows: any[];
@@ -14,23 +16,34 @@ const DeleteSelectedMailAccounts = ({
   selectedRows,
   onLoadApi,
 }: DeleteSelectedMailAccountsTypes) => {
+  const [open, setOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = useState<any>({
+    submit: false,
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const onDelete = async () => {
     devLog("selectedRows", selectedRows);
     if (!selectedRows?.length) {
       ToastWarning("Please select mail account");
       return;
     }
+    setIsLoading((prev: any) => ({ ...prev, submit: true }));
     selectedRows.map(async (item: any, idx) => {
       try {
         let res = await deleteMailAccountService(item?.id);
         // if (res?.status === 204) {}
         if (idx === selectedRows?.length - 1) {
           ToastSuccess("Selected mail account successfully deleted.");
+          setIsLoading((prev: any) => ({ ...prev, submit: false }));
           onLoadApi();
         }
       } catch ({ response }) {
         ToastError("Something went wrong!");
         devLogError(response);
+        setIsLoading((prev: any) => ({ ...prev, submit: false }));
       }
     });
   };
@@ -39,7 +52,7 @@ const DeleteSelectedMailAccounts = ({
     <>
       <Tooltip title="Delete selected mail accounts">
         <Button
-          onClick={onDelete}
+          onClick={handleOpen}
           disabled={false}
           className={`${
             selectedRows?.length > 0 ? "tw-text-[#1a76d2]" : "tw-text-[#778da9]"
@@ -55,6 +68,40 @@ const DeleteSelectedMailAccounts = ({
           Delete
         </Button>
       </Tooltip>
+
+      {open && (
+        <MyModal
+          open={open}
+          onClose={handleClose}
+          modalTitle="Are you sure?"
+          labelledby="Are you sure?"
+          describedby="delete selected mail account modal"
+          modalSxStyle={{ width: { xs: 400 } }}
+        >
+          <Typography variant="subtitle1">
+            You won't be able to revert this!
+          </Typography>
+          <DialogActions>
+            <LoadingButton
+              variant="contained"
+              color="primary"
+              onClick={onDelete}
+              loading={isLoading?.submit}
+              disabled={isLoading?.submit}
+              className="tw-bg-primary hover:tw-bg-primaryDark tw-normal-case"
+            >
+              Yes, please!
+            </LoadingButton>
+            <LoadingButton
+              onClick={handleClose}
+              loading={isLoading?.submit}
+              disabled={isLoading?.submit}
+            >
+              Cancel
+            </LoadingButton>
+          </DialogActions>
+        </MyModal>
+      )}
     </>
   );
 };
--- src/ui-component/buttons/DeleteSelectedPeople.tsx
+++ src/ui-component/buttons/DeleteSelectedPeople.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
-import { Button, Tooltip } from "@mui/material";
+import { Button, DialogActions, Tooltip, Typography } from "@mui/material";
 import { devLog, devLogError } from "../../helpers/logs";
 import { deletePersonDetailService } from "../../services/persons.service";
 import { ToastError, ToastSuccess, ToastWarning } from "../../helpers/toast";
+import MyModal from "../modal/MyModal";
+import { LoadingButton } from "@mui/lab";
 
 type DeleteSelectedPeopleTypes = {
   selectedRows: any[];
@@ -14,23 +16,34 @@ const DeleteSelectedPeople = ({
   selectedRows,
   onLoadApi,
 }: DeleteSelectedPeopleTypes) => {
+  const [open, setOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = useState<any>({
+    submit: false,
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const onDelete = async () => {
     devLog("selectedRows", selectedRows);
     if (!selectedRows?.length) {
       ToastWarning("Please select person");
       return;
     }
+    setIsLoading((prev: any) => ({ ...prev, submit: true }));
     selectedRows.map(async (item: any, idx) => {
       try {
         let res = await deletePersonDetailService(item?.id);
         // if (res?.status === 204) {}
         if (idx === selectedRows?.length - 1) {
           ToastSuccess("Selected people successfully deleted.");
+          setIsLoading((prev: any) => ({ ...prev, submit: false }));
           onLoadApi();
         }
       } catch ({ response }) {
         ToastError("Something went wrong!");
         devLogError(response);
+        setIsLoading((prev: any) => ({ ...prev, submit: false }));
       }
     });
   };
@@ -39,7 +52,7 @@ const DeleteSelectedPeople = ({
     <>
       <Tooltip title="Delete selected people">
         <Button
-          onClick={onDelete}
+          onClick={handleOpen}
           disabled={false}
           className={`${
             selectedRows?.length > 0 ? "tw-text-[#1a76d2]" : "tw-text-[#778da9]"
@@ -55,6 +68,40 @@ const DeleteSelectedPeople = ({
           Delete
         </Button>
       </Tooltip>
+
+      {open && (
+        <MyModal
+          open={open}
+          onClose={handleClose}
+          modalTitle="Are you sure?"
+          labelledby="Are you sure?"
+          describedby="delete selected people modal"
+          modalSxStyle={{ width: { xs: 400 } }}
+        >
+          <Typography variant="subtitle1">
+            You won't be able to revert this!
+          </Typography>
+          <DialogActions>
+            <LoadingButton
+              variant="contained"
+              color="primary"
+              onClick={onDelete}
+              loading={isLoading?.submit}
+              disabled={isLoading?.submit}
+              className="tw-bg-primary hover:tw-bg-primaryDark tw-normal-case"
+            >
+              Yes, please!
+            </LoadingButton>
+            <LoadingButton
+              onClick={handleClose}
+              loading={isLoading?.submit}
+              disabled={isLoading?.submit}
+            >
+              Cancel
+            </LoadingButton>
+          </DialogActions>
+        </MyModal>
+      )}
     </>
   );
 };
