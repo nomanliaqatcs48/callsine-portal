875b6776d48abd69693e9b91d716ec74086245be
kenvilar <kenvilar@gmail.com>
2023-06-14 00:02:37 +0800

    hide full name column

--- src/utils/user-data/utils.tsx
+++ src/utils/user-data/utils.tsx
@@ -4,13 +4,6 @@ export const _columns = () => {
   return useMemo(
     () => [
       {
-        Header: "Full Name",
-        accessor: "user.first_name",
-        Cell: (cell: any) => {
-          return `${cell?.value} ${cell?.row?.original?.user?.last_name}`;
-        },
-      },
-      {
         Header: "File",
         accessor: "file",
       },
