7d5b94280b29fe97f8bd9abbb194d845c7f2b73f
kenvilar <kenvilar@gmail.com>
2023-06-22 22:18:57 +0800

    add updateProspectSequenceEventDetailService

--- src/services/sequences.service.tsx
+++ src/services/sequences.service.tsx
@@ -5,3 +5,12 @@ import { endpoints } from "./endpoints";
 export const getProspectSequenceEventService = async (personId: string) => {
   return await http.get(`${endpoints.SEQUENCE_EVENTS}?person.id=${personId}`);
 };
+
+export const updateProspectSequenceEventDetailService = async (
+  id: number,
+  personId: string
+) => {
+  return await http.get(
+    `${endpoints.SEQUENCE_EVENTS}${id}/?person.id=${personId}`
+  );
+};
