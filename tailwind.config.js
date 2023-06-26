c12f16826262b292a006c25ec8a9c685410a9623
kenvilar <kenvilar@gmail.com>
2023-06-26 16:22:29 +0800

    add tailwind default screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   prefix: "tw-",
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       screens: {
         "3xl": "1800px",
