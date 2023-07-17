12c6c725609fc298ac72106fb14749c74c2f53db
kenvilar <kenvilar@gmail.com>
2023-07-17 20:03:58 +0800

    Fix email sent value

--- src/views/persons/Detail/components/ProfileFirstCol.tsx
+++ src/views/persons/Detail/components/ProfileFirstCol.tsx
@@ -85,7 +85,7 @@ const ProfileFirstCol = ({ data }: ProfileFirstColTypes) => {
             Emails Sent
           </Typography>
           <Typography className="tw-flex tw-flex-row tw-justify-center tw-font-semibold tw-text-lg tw-text-black">
-            {personStatData?.emails_sent || "0"}
+            {data?.sent_emails || "0"}
           </Typography>
         </div>
         <div>
