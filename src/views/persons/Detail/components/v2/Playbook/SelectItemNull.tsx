aef87cabfbea422d96d3bc878374ebb64168f899
kenvilar <kenvilar@gmail.com>
2023-07-23 12:02:58 +0800

    Add props for SelectItemNull component

---
+++ src/views/persons/Detail/components/v2/Playbook/SelectItemNull.tsx
@@ -0,0 +1,24 @@
+type SelectItemTypes = {
+  prompts: any[];
+  stringForEmpty: string;
+  stringForNotEmpty: string;
+};
+
+const SelectItemNull = ({
+  prompts,
+  stringForEmpty,
+  stringForNotEmpty,
+}: SelectItemTypes) => {
+  return (
+    <>
+      <div className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-h-full">
+        <div className="tw-text-xl tw-py-10 tw-text-[#99a9be] tw-font-semibold">
+          {prompts?.length === 0 && stringForEmpty}
+          {prompts?.length > 0 && stringForNotEmpty}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SelectItemNull;
