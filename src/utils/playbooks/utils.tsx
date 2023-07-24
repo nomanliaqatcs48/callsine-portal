3e876c3dee8bce971e99f1fcb8be5e9093da7bf7
kenvilar <kenvilar@gmail.com>
2023-07-24 15:17:13 +0800

    Add labelValueInput util

--- src/utils/playbooks/utils.tsx
+++ src/utils/playbooks/utils.tsx
@@ -5,6 +5,8 @@ export const _styles = {
     "tw-w-4/12 tw-font-light tw-text-[#B9B9B9] lg:tw-w-3/12 xl:tw-w-2/12 2xl:tw-w-1/12 tw-flex tw-items-center",
   labelValue:
     "tw-w-8/12 tw-font-normal tw-truncate lg:tw-w-9/12 xl:tw-w-10/12 2xl:tw-w-11/12 2xl:tw-pl-4",
+  labelValueInput:
+    "tw-w-full tw-outline-none tw-border-b tw-border-transparent tw-transition-all tw-duration-500 focus:tw-border-b focus:tw-border-[#569ade]",
   message:
     "tw-w-full tw-outline-none tw-border-b tw-border-transparent tw-transition-all tw-duration-500 focus:tw-border-b focus:tw-border-[#569ade]",
 };
