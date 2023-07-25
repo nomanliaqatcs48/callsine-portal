9ba7d3be9c2a4a79160d2d94a6330129681f1a47
kenvilar <kenvilar@gmail.com>
2023-07-25 16:25:05 +0800

    Add poppins specific weights

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import "@fontsource/poppins";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import "@fontsource/roboto";
 import "@fontsource/inter";
 
