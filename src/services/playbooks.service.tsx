afe29c52e5d26194a8171dc244ff0d4dfac440c2
kenvilar <kenvilar@gmail.com>
2023-08-01 12:10:42 +0800

    Add slash on getTeamPlaybooks api endpoint

--- src/services/playbooks.service.tsx
+++ src/services/playbooks.service.tsx
@@ -2,5 +2,5 @@ import http from "./axios";
 import { endpoints } from "./endpoints";
 
 export const getTeamPlaybooks = async (teamId: number) => {
-  return await http.get(`${endpoints.TEAMS}${teamId}/playbooks`);
+  return await http.get(`${endpoints.TEAMS}${teamId}/playbooks/`);
 };
