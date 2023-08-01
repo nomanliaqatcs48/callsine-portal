1c4a1a2206270ac7f6e16d98c66210228ca6e4c1
lkfuel <logan@fuelgrowth.co>
2023-08-01 13:04:57 -0400

    finished playbooks

--- src/hooks/playbook/useReloadPlaybooks.ts
+++ src/hooks/playbook/useReloadPlaybooks.ts
@@ -1,14 +1,16 @@
-import { useAuth } from "../../contexts/auth";
 import { useDispatch } from "react-redux";
-import { setPlaybookData } from "../../store/playbooks/actions";
+import { useAuth } from "../../contexts/auth";
 import { getTeamPlaybooks } from "../../services/playbooks.service";
+import { setPlaybookData } from "../../store/playbooks/actions";
 
 export const useReloadPlaybooks = () => {
   const dispatch = useDispatch();
   const auth: any = useAuth();
+  console.log("AUTHENTICATIOn");
+  console.log(auth);
 
   const fetchPlaybooks = async () => {
-    const newPlaybooks = await getTeamPlaybooks(auth["team"]);
+    const newPlaybooks = await getTeamPlaybooks(auth["team"], "", "");
     dispatch(setPlaybookData(newPlaybooks.data?.results));
   };
 
