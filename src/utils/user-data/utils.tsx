9958a936dc07de5b36fdbf92d2fff1af832963c5
kenvilar <kenvilar@gmail.com>
2023-08-07 17:24:04 +0800

    Make a link button for data file url

--- src/utils/user-data/utils.tsx
+++ src/utils/user-data/utils.tsx
@@ -2,15 +2,38 @@ import React, { useMemo } from "react";
 import { IconTrash } from "@tabler/icons-react";
 import DeleteUserData from "../../ui-component/buttons/DeleteUserData";
 import { useTheme } from "@mui/material/styles";
+import { Button } from "@mui/material";
 
 export const _columns = (onLoadApi: any) => {
   const theme: any = useTheme();
+
+  const downloadFile = (uri: any, name: any) => {
+    let link = document.createElement("a");
+    // If you don't know the name or want to use
+    // the webserver default set name = ''
+    link.setAttribute("download", name);
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  };
 
   return useMemo(
     () => [
       {
         Header: "File",
         accessor: "file",
+        Cell: (cell: any) => {
+          return (
+            <Button
+              variant="text"
+              className="tw-normal-case"
+              onClick={() => downloadFile(cell?.value, "file")}
+            >
+              {cell?.value || ""}
+            </Button>
+          );
+        },
       },
       {
         Header: "Actions",
