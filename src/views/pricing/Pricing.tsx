765893962453e170109a9690ac3eed0128788221
kenvilar <kenvilar@gmail.com>
2023-08-10 14:46:31 +0800

    Final pricing page

--- src/views/pricing/Pricing.tsx
+++ src/views/pricing/Pricing.tsx
@@ -29,7 +29,7 @@ const PricingBox = () => {
 
   const CustomHeader = ({ children, ...props }: any) => {
     return (
-      <Typography className="tw-text-[24px] tw-text-[#0e101a] tw-font-bold tw-leading-[32px] tw-mb-3">
+      <Typography className="tw-text-[24px] tw-text-[#0e101a] tw-font-bold tw-leading-[32px] tw-mb-3 tw-text-center">
         {children}
       </Typography>
     );
@@ -48,7 +48,7 @@ const PricingBox = () => {
       <Button
         variant="contained"
         color="primary"
-        className="tw-text-[14px] tw-font-bold tw-w-full tw-py-[12px] tw-bg-primary hover:tw-bg-primaryDark "
+        className="tw-text-[14px] tw-font-bold tw-w-full tw-py-[12px] tw-my-[20px] tw-bg-primary hover:tw-bg-primaryDark "
       >
         {children}
       </Button>
@@ -59,51 +59,66 @@ const PricingBox = () => {
     <>
       <Grid
         container
-        className="tw-bg-[#fafafa] tw-relative tw-overflow-hidden tw-min-h-[100vh] 2xl:tw-px-[180px] 3xl:tw-px-[240px]"
+        className="tw-bg-[#fafafa] tw-overflow-hidden tw-min-h-[80vh] 2xl:tw-px-[180px] 3xl:tw-px-[240px]"
+        sx={{
+          backgroundImage: `url('${imageBgc}')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
       >
-        <img
-          className="tw-h-auto tw-max-w-full"
-          src={imageBgcTop}
-          alt="description"
-          style={{
-            position: "absolute",
-            zIndex: "999",
-            minHeight: "calc(100vh + 92px)",
-            objectFit: "cover",
-            width: "100%",
-            mixBlendMode: "overlay",
-            opacity: "0.4",
-          }}
-        />
-        <img
-          className="tw-h-auto tw-max-w-full"
-          src={imageBgc}
-          alt="description"
-          style={{
-            position: "absolute",
-            minHeight: "calc(100vh + 92px)",
-            width: "100%",
-          }}
-        />
-        <Box className="tw-flex tw-flex-col tw-gap-5 tw-pt-[150px] tw-px-3 tw-w-full sm:tw-px-20 lg:tw-pt-[87px]">
-          <CustomGrid>
-            <CustomCard>
-              <CustomHeader>Monthly Plan</CustomHeader>
-              <CustomDesc>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </CustomDesc>
-              <CustomButton>Get Started</CustomButton>
-            </CustomCard>
-          </CustomGrid>
-          <CustomGrid>
-            <CustomCard>
-              <CustomHeader>Yearly Plan</CustomHeader>
-              <CustomDesc>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </CustomDesc>
-              <CustomButton>Get Started</CustomButton>
-            </CustomCard>
-          </CustomGrid>
+        <Box className="tw-pt-[150px] tw-px-3 sm:tw-px-20">
+          <Box className="content">
+            <Typography
+              variant="h1"
+              className="tw-text-white tw-text-[40px] tw-leading-[48px] tw-text-center tw-mb-[40px]"
+            >
+              🎉 Introducing CallSine's Flexible Subscription Plans!
+            </Typography>
+            <Typography
+              variant="body2"
+              className="tw-text-white tw-text-center tw-text-[20px] tw-font-normal tw-leading-[24px] tw-mb-[40px]"
+            >
+              At CallSine, we believe in empowering your marketing and sales
+              teams like never before. We've crafted two subscription options
+              tailored to your needs and budget, ensuring that you get the best
+              value and functionality, without compromise.
+            </Typography>
+          </Box>
+
+          <Box className="pricing-boxes tw-flex tw-flex-col tw-gap-5 tw-w-full md:tw-flex-row">
+            <CustomGrid>
+              <CustomCard>
+                <CustomHeader>Monthly Freedom</CustomHeader>
+                <CustomDesc>
+                  $99 per seat per month: Ideal for those who prefer flexibility
+                  and wish to adapt to changing needs. This option lets you
+                  enjoy all the premium features, support, and seamless
+                  experience of CallSine, billed on a convenient monthly basis.
+                  No strings attached!
+                </CustomDesc>
+                <CustomButton>Get Started</CustomButton>
+              </CustomCard>
+            </CustomGrid>
+            <CustomGrid>
+              <CustomCard>
+                <CustomHeader>Annual Savings</CustomHeader>
+                <CustomDesc>
+                  $999 per seat per year: Ready to commit to excellence? Opt for
+                  our yearly subscription and save big. With a one-time payment,
+                  you'll get the full suite of CallSine features for an entire
+                  year. That's like getting two months FREE compared to the
+                  monthly plan.
+                </CustomDesc>
+                <CustomButton>Get Started</CustomButton>
+              </CustomCard>
+            </CustomGrid>
+          </Box>
+
+          <Box className="footer-content tw-text-white tw-text-[15px] tw-font-normal tw-leading-[18px] tw-pt-[40px] tw-pb-[80px] tw-text-center">
+            Whatever your preference, both plans guarantee CallSine's
+            unparalleled quality, innovation, and customer support. Sign up
+            today, and let's transform the way you connect!
+          </Box>
         </Box>
       </Grid>
     </>
--- src/views/pricing/index.tsx
+++ src/views/pricing/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PricingBox from "./Pricing";
+import Footer from "../home/Footer";
 
 const PricingPage = () => {
   return (
     <>
       <PricingBox />
+      <Footer />
     </>
   );
 };
