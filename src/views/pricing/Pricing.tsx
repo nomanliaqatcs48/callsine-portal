ceea01ece6da394964c9f905719e08066239ba4e
kenvilar <kenvilar@gmail.com>
2023-08-11 23:59:50 +0800

    Separate component

--- src/views/pricing/Pricing.tsx
+++ src/views/pricing/Pricing.tsx
@@ -3,6 +3,7 @@ import { Box, Grid, Typography } from "@mui/material";
 import imageBgc from "../../assets/images/users/Rectangle1.png";
 import PricingCard from "../../ui-component/pricing/PricingCard";
 import PageHeader from "./PageHeader";
+import PricingBottomContent from "./PricingBottomContent";
 
 const PricingBox = () => {
   const pricingItems = [
@@ -84,11 +85,7 @@ const PricingBox = () => {
             })}
           </Box>
 
-          <Box className="footer-content tw-text-white tw-text-[18px] tw-font-normal tw-leading-[31px] tw-py-[100px] tw-text-center xl:tw-px-[100px]">
-            Whatever your preference, both plans guarantee CallSine's
-            unparalleled quality, innovation, and customer support. Sign up
-            today, and let's transform the way you connect!
-          </Box>
+          <PricingBottomContent />
         </Box>
       </Grid>
     </>
