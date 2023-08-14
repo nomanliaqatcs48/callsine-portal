0cbffc28bfe908f22311d42bc4fff1398badf713
kenvilar <kenvilar@gmail.com>
2023-08-14 22:42:52 +0800

    Update BlogImage

--- src/ui-component/blog/BlogImage.tsx
+++ src/ui-component/blog/BlogImage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Box } from "@mui/material";
 
-const BlogImage = ({ image, ...props }: any) => {
+const BlogImage = ({ src, ...props }: any) => {
   return (
     <Box className="" {...props}>
       <img
-        src={image || "https://picsum.photos/id/7/800/600"}
+        src={src || "https://picsum.photos/id/7/800/600"}
         alt="featured-img"
         className="tw-max-w-full tw-h-full tw-mx-auto lg:tw-mb-[50px]"
       />
