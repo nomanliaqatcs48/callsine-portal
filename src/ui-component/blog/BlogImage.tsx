9203aa383f515985bf25b6acfab14e2eb556820f
kenvilar <kenvilar@gmail.com>
2023-08-15 03:45:21 +0800

    Minor

--- src/ui-component/blog/BlogImage.tsx
+++ src/ui-component/blog/BlogImage.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 
 const BlogImage = ({ src, ...props }: any) => {
   return (
-    <Box className="" {...props}>
+    <Box className="tw-mb-5 lg:tw-h-[500px]" {...props}>
       <img
         src={src || "https://picsum.photos/id/7/800/600"}
         alt="featured-img"
