8dbd5e961d09eb513e568048a679e98a74ac479f
kenvilar <kenvilar@gmail.com>
2023-08-15 17:53:51 +0800

    Minor

--- src/utils/pricing/utils.tsx
+++ src/utils/pricing/utils.tsx
@@ -167,4 +167,40 @@ export const compareFeaturesItems = [
       },
     ],
   },
+  {
+    title: "CallSine Analytics",
+    items: [
+      {
+        plan: "Monthly Freedom",
+        desc: "",
+      },
+      {
+        plan: "Volume Discount",
+        desc: "",
+      },
+      {
+        plan: "Enterprise Elevation",
+        desc: (
+          <span
+            className="tw-flex tw-justify-center tw-items-center"
+            style={{
+              backgroundColor: "#00875a",
+              display: "flex",
+              width: 24,
+              height: 24,
+              lineHeight: 24,
+              color: "#fff",
+              fontWeight: "bold",
+              borderRadius: "50%",
+              flexShrink: 0,
+              margin: 4,
+            }}
+          >
+            {" "}
+            ✓{" "}
+          </span>
+        ),
+      },
+    ],
+  },
 ];
