beacf4e63c8e09735c4ede090c28a31e14c05619
kenvilar <kenvilar@gmail.com>
2023-08-15 15:37:20 +0800

    Separate items into utils

---
+++ src/utils/pricing/utils.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { useAuthentication } from "../../hooks/useAuthentication";
+
+export const pricingItems = ({ authProfile }: any) => [
+  {
+    hide: true,
+    name: "",
+    price: "",
+    description: "",
+    onClick: () => null,
+    buttontext: "",
+    bottomContent: "",
+    isPopular: false,
+  },
+  {
+    name: "Monthly Freedom",
+    price: (
+      <>
+        <Box className="tw-text-center">
+          <strong className="tw-text-[30px]">$99</strong> <br />
+          per seat per month
+        </Box>
+      </>
+    ),
+    description:
+      "Ideal for those who prefer flexibility and wish to adapt to changing needs. This option lets you enjoy all the premium features, support, and seamless experience of CallSine, billed on a convenient monthly basis. No strings attached!",
+    onClick: () => {
+      window.open(
+        `https://kvilar-unionresolute.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=Premium-Plan-USD-Monthly&customer[email]=${authProfile?.email}&utm_source=cb-app-copy`
+      );
+    },
+    buttontext: "Get Started",
+    bottomContent: "For growing teams focused on building more together",
+    isPopular: false,
+  },
+  {
+    name: "Volume Discount",
+    price: (
+      <>
+        <Box className="tw-text-center">
+          <strong className="tw-text-[30px]">$79</strong> <br />
+          per seat per month
+        </Box>
+      </>
+    ),
+    description:
+      "Opt for our multi-seat subscription and save. if you have a need for 5 to 20 seats, you will get a $20 per month discount for all seats. That's a $240 savings per peat per year.  And it’s still month to month, no contract.",
+    onClick: () => {
+      window.open(
+        `https://kvilar-unionresolute.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=Premium-Plan-USD-Yearly&customer[email]=${authProfile?.email}&utm_source=cb-app-copy`
+      );
+    },
+    buttontext: "Get Started",
+    bottomContent:
+      "For organization that need to scale how they collaborate and track work",
+    isPopular: true,
+  },
+  {
+    name: "Enterprise Elevation",
+    price: (
+      <>
+        <Box className="tw-text-center">
+          <strong className="tw-text-[30px]">Custom Pricing</strong>
+        </Box>
+      </>
+    ),
+    description:
+      "For organizations requiring over 20 seats, our Enterprise Elevation plan offers custom-tailored features to meet your unique needs. Contact us to discuss your specific requirements, and we'll design a package that fits just right.",
+    onClick: () => {
+      //
+    },
+    buttontext: "Contact Sales",
+    bottomContent:
+      "For enterprises with global scale, security, and governance needs",
+    isPopular: false,
+  },
+];
--- src/views/pricing/Pricing.tsx
+++ src/views/pricing/Pricing.tsx
@@ -5,85 +5,11 @@ import PricingCard from "../../ui-component/pricing/PricingCard";
 import PageHeader from "./PageHeader";
 import PricingBottomContent from "./PricingBottomContent";
 import { useAuthentication } from "../../hooks/useAuthentication";
+import { pricingItems } from "../../utils/pricing/utils";
 // import { ChargeBee, _hosted_page } from "chargebee-typescript";
 
 const PricingBox = () => {
   let { isAuthenticated, authProfile } = useAuthentication();
-
-  const pricingItems = [
-    {
-      hide: true,
-      name: "",
-      price: "",
-      description: "",
-      onClick: () => null,
-      buttontext: "",
-      bottomContent: "",
-      isPopular: false,
-    },
-    {
-      name: "Monthly Freedom",
-      price: (
-        <>
-          <Box className="tw-text-center">
-            <strong className="tw-text-[30px]">$99</strong> <br />
-            per seat per month
-          </Box>
-        </>
-      ),
-      description:
-        "Ideal for those who prefer flexibility and wish to adapt to changing needs. This option lets you enjoy all the premium features, support, and seamless experience of CallSine, billed on a convenient monthly basis. No strings attached!",
-      onClick: () => {
-        window.open(
-          `https://kvilar-unionresolute.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=Premium-Plan-USD-Monthly&customer[email]=${authProfile?.email}&utm_source=cb-app-copy`
-        );
-      },
-      buttontext: "Get Started",
-      bottomContent: "For growing teams focused on building more together",
-      isPopular: false,
-    },
-    {
-      name: "Volume Discount",
-      price: (
-        <>
-          <Box className="tw-text-center">
-            <strong className="tw-text-[30px]">$79</strong> <br />
-            per seat per month
-          </Box>
-        </>
-      ),
-      description:
-        "Opt for our multi-seat subscription and save. if you have a need for 5 to 20 seats, you will get a $20 per month discount for all seats. That's a $240 savings per peat per year.  And it’s still month to month, no contract.",
-      onClick: () => {
-        window.open(
-          `https://kvilar-unionresolute.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=Premium-Plan-USD-Yearly&customer[email]=${authProfile?.email}&utm_source=cb-app-copy`
-        );
-      },
-      buttontext: "Get Started",
-      bottomContent:
-        "For organization that need to scale how they collaborate and track work",
-      isPopular: true,
-    },
-    {
-      name: "Enterprise Elevation",
-      price: (
-        <>
-          <Box className="tw-text-center">
-            <strong className="tw-text-[30px]">Custom Pricing</strong>
-          </Box>
-        </>
-      ),
-      description:
-        "For organizations requiring over 20 seats, our Enterprise Elevation plan offers custom-tailored features to meet your unique needs. Contact us to discuss your specific requirements, and we'll design a package that fits just right.",
-      onClick: () => {
-        //
-      },
-      buttontext: "Contact Sales",
-      bottomContent:
-        "For enterprises with global scale, security, and governance needs",
-      isPopular: false,
-    },
-  ];
 
   return (
     <>
@@ -102,7 +28,7 @@ const PricingBox = () => {
           <Box className="tw-py-[15px]" />
 
           <Box className="pricing-boxes tw-flex tw-flex-col tw-items-stretch tw-gap-5 lg:tw-gap-0 lg:tw-flex-row">
-            {pricingItems?.map((i: any, idx: number) => {
+            {pricingItems({ authProfile })?.map((i: any, idx: number) => {
               return (
                 <Box className="tw-flex-1">
                   <PricingCard
