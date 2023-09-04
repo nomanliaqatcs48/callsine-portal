3c20ee7ed88ad3d0e09d833f64445ad14b07a1dd
Gene M <eug5ne@gmail.com>
2023-09-04 21:18:37 +0800

    feat: wizard

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -25,6 +25,10 @@ const PlaybooksPage = Loadable(lazy(() => import("../views/playbooks")));
 const SamplePage = Loadable(lazy(() => import("../views/sample-page")));
 const TeamPage = Loadable(lazy(() => import("../views/team")));
 
+const WizardPage = Loadable(lazy(() => import("../views/wizard")));
+
+const WizardPageOld = Loadable(lazy(() => import("../views/wizard/old")));
+
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
@@ -86,6 +90,14 @@ const MainRoutes = {
     {
       path: "sample-page",
       element: <SamplePage />,
+    },
+    {
+      path: "wizard",
+      element: <WizardPage />,
+    },
+    {
+      path: "wizard/old",
+      element: <WizardPageOld />,
     },
   ],
 };
