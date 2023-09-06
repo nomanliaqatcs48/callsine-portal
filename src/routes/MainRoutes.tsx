4b33c24d2928265985980b55172d171352924a88
Gene M <eug5ne@gmail.com>
2023-09-06 17:54:37 +0800

    fix: move wizard to auth public route

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -25,10 +25,6 @@ const PlaybooksPage = Loadable(lazy(() => import("../views/playbooks")));
 const SamplePage = Loadable(lazy(() => import("../views/sample-page")));
 const TeamPage = Loadable(lazy(() => import("../views/team")));
 
-const WizardPage = Loadable(lazy(() => import("../views/wizard")));
-
-const WizardPageOld = Loadable(lazy(() => import("../views/wizard/old")));
-
 // ==============================|| MAIN ROUTING ||============================== //
 
 const MainRoutes = {
@@ -90,14 +86,6 @@ const MainRoutes = {
     {
       path: "sample-page",
       element: <SamplePage />,
-    },
-    {
-      path: "wizard",
-      element: <WizardPage />,
-    },
-    {
-      path: "wizard/old",
-      element: <WizardPageOld />,
     },
   ],
 };
