4937146aded36f501c5fa95270fae6c205eff624
Gene M <eug5ne@gmail.com>
2023-09-15 05:34:32 +0800

    feat: Main component

---
+++ src/views/checkout/components/Main.tsx
@@ -0,0 +1,25 @@
+import { styled } from "@mui/material";
+import { drawerWidth } from "../constants";
+
+export const Main = styled("main", {
+  shouldForwardProp: (prop) => prop !== "open",
+})<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  // width: `calc(100%)`,
+  marginRight: -drawerWidth,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+    // width: `calc(100% - ${drawerWidth}px)`,
+  }),
+}));
