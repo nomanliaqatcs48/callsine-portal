770c06772375b89cf228ac84b456ed7ed93f8626
Eugene <eug5ne@gmail.com>
2023-09-16 19:57:23 +0800

    fix: maintain the left margin even on open sided bar

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -55,7 +55,7 @@ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
-      marginLeft: 0,
+      marginLeft: "20px",
       borderBottomLeftRadius: 0,
       borderBottomRightRadius: 0,
       width: `calc(100% - ${drawerWidth}px)`,
