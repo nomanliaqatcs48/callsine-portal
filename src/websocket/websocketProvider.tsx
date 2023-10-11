c7b70381f4e392f9f9a7408f24ceefd5f6c2cb53
Marc <marcmaligmat@gmail.com>
2023-10-11 21:40:54 +0800

    changed url ws

--- src/websocket/websocketProvider.tsx
+++ src/websocket/websocketProvider.tsx
@@ -14,7 +14,7 @@ interface WebsocketProviderProps {
 }
 
 const useWebsocket = (id: string) => {
-  const socketUrl = `ws://api.callsine.com/ws/users/${id}/`;
+  const socketUrl = `wss://api.callsine.com/ws/users/${id}/`;
   const socket = useMemo(() => {
     return new ReconnectingWebSocket(socketUrl);
   }, [socketUrl]);
