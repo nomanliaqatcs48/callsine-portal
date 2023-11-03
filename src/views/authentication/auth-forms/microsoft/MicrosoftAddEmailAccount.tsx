7f29dec49c325583838672f4274a1385bb34af37
Marc <marcmaligmat@gmail.com>
2023-11-04 00:12:13 +0800

    removed prompt consent

--- src/views/authentication/auth-forms/microsoft/MicrosoftAddEmailAccount.tsx
+++ src/views/authentication/auth-forms/microsoft/MicrosoftAddEmailAccount.tsx
@@ -7,7 +7,7 @@ const MicrosoftAddEmailAccount = () => {
   const REDIRECT_URI = config.microsoftRedirectUri;
   const SCOPE =
     "User.Read+email+offline_access+openid+profile+Mail.ReadWrite+Mail.ReadWrite.Shared+Contacts.ReadWrite+Mail.Send";
-  const MS_AUTH_URL = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&response_mode=query&scope=${SCOPE}&state=${state}&prompt=consent`;
+  const MS_AUTH_URL = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&response_mode=query&scope=${SCOPE}&state=${state}`;
 
   return (
     <div>
