09148fe5c1ab39602e98a81c5839f031540b4d95
Noman Liaqat <noman.liaqat@invozone.com>
2023-11-23 23:48:54 +0500

    Inbox - Thread flipping issue

--- src/views/inbox/components/Email.tsx
+++ src/views/inbox/components/Email.tsx
@@ -2,17 +2,23 @@ import * as React from "react";
 
 import { Avatar, Grid, Stack, Typography } from "@mui/material";
 import { stringAvatar } from "src/helpers/strings";
-
+import { formatDate } from "src/utils/date";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { useUnreadCount } from "src/hooks/useUnreadCount";
 
 interface EmailProps {
   item: any;
   index: number;
+  handleOnClickEmail: any;
   isSelected: boolean;
+  handleSelectThread: any;
+  selectedEmail: number;
+  handleSubjectBoldFont: any
 }
 
-const Email: React.FC<EmailProps> = ({ item, index, isSelected = false }) => {
+const Email: React.FC<EmailProps> = ({ item, index, handleOnClickEmail, isSelected = false, handleSelectThread, selectedEmail, handleSubjectBoldFont}) => {
   const { unreadEmails } = useUnreadCount();
 
   const handleSubjectBold = (item: any) => {
@@ -32,6 +38,7 @@ const Email: React.FC<EmailProps> = ({ item, index, isSelected = false }) => {
   };
 
   return (
+    <>
     <Stack
       key={item.recipient}
       direction="row"
@@ -41,6 +48,7 @@ const Email: React.FC<EmailProps> = ({ item, index, isSelected = false }) => {
       spacing={2}
       maxWidth={450}
       minWidth={450}
+      onClick={() => handleOnClickEmail()}
       className="tw-cursor-pointer tw-border-b tw-border-[#f0f1f3] hover:tw-bg-gray-100 tw-transition-colors tw-duration"
     >
       <Stack direction="row" spacing={1}>
@@ -63,14 +71,51 @@ const Email: React.FC<EmailProps> = ({ item, index, isSelected = false }) => {
       </Stack>
       <span className="tw-px-2 tw-text-left tw-flex tw-justify-between tw-text-sm">
         <span
-          className={`tw-ml-2 tw-text-gray-300 ${
-            isSelected ? "tw-rotate-180" : ""
-          } tw-transition-transform`}
+          className={`tw-ml-2 tw-text-gray-300 tw-transition-transform`}
         >
-          <ArrowForwardIosIcon />
+          {isSelected ? <ExpandLessIcon /> : <ExpandMoreIcon /> }
         </span>
       </span>
+      
     </Stack>
+    {isSelected && item?.emails?.length > 0 && item.emails.filter((email: any) => email.status === 0) //only show status = 0 "sent"
+    .map((email: any, index: number) => (
+      <Stack
+        py={2}
+        px={3}
+        key={`${email.subject}-${index}`}
+        onClick={() => handleSelectThread(email, index)}
+        className={`tw-w-full tw-border-b tw-border-[#f0f1f3] tw-cursor-pointer hover:tw-bg-gray-100 tw-transition-colors tw-duration-300 ${
+          selectedEmail === index &&
+          `tw-border-l-[10px] tw-border-l-primary`
+        }`}
+      >
+        <Stack direction="row" justifyContent="space-between">
+          {/* <Typography className="tw-font-medium tw-text-[16px]"> */}
+          <Typography
+            className={`tw-text-xs ${
+              handleSubjectBoldFont(email)
+                ? "tw-font-medium"
+                : "tw-font-thin"
+            }`}
+          >
+            {email.subject}
+          </Typography>
+          <Typography
+            className={`tw-text-xs tw-w-[80px] ${
+              handleSubjectBoldFont(email)
+                ? "tw-font-medium"
+                : "tw-font-thin"
+            }`}
+          >
+            {formatDate(email.modified_date)}
+          </Typography>
+        </Stack>
+        <br />
+        {/*  */}
+      </Stack>
+    ))}
+    </>
   );
 };
 
